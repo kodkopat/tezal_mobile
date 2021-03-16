9c41eead2de85642b549e23fd23a4e7a2aaffd52
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-16 18:52:48 +0330

    create customized future builder

---
+++ lib/core/widgets/custom_future_builder.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomFutureBuilder<T> extends StatelessWidget {
+  CustomFutureBuilder({
+    @required this.future,
+    @required this.successBuilder,
+    @required this.errorBuilder,
+  });
+
+  final Future<T> future;
+  final Widget Function(BuildContext, T) successBuilder;
+  final Widget Function(BuildContext, String) errorBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<T>(
+      future: future,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            !snapshot.hasError) {
+          return successBuilder(context, snapshot.data);
+        } else {
+          return errorBuilder(context, "${snapshot.error}");
+        }
+      },
+    );
+  }
+}
