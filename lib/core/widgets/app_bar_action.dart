1a51f14596e7a6bfb9d0723a782eac62e269ec76
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-16 18:56:23 +0330

    create appBar Action widget

---
+++ lib/core/widgets/app_bar_action.dart
@@ -0,0 +1,47 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+import '../../core/page_routes/routes.dart';
+import '../../core/styles/txt_styles.dart';
+
+class AppBarAction extends StatelessWidget {
+  AppBarAction({
+    @required this.text,
+    this.showBackBtn,
+  });
+
+  final String text;
+  final bool showBackBtn;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      textDirection: TextDirection.rtl,
+      children: [
+        (showBackBtn ?? false)
+            ? Parent(
+                gesture: Gestures()..onTap(() => Routes.sailor.pop()),
+                style: ParentStyle()
+                  ..width(48)
+                  ..height(48)
+                  ..margin(all: 4)
+                  ..borderRadius(all: 24)
+                  ..ripple(true),
+                child: Icon(
+                  Feather.arrow_right,
+                ),
+              )
+            : SizedBox(width: 16),
+        Txt(
+          text,
+          style: AppTxtStyles().subHeading
+            ..textColor(Theme.of(context).iconTheme.color)
+            ..textOverflow(TextOverflow.ellipsis)
+            ..maxLines(1)
+            ..bold(),
+        ),
+      ],
+    );
+  }
+}
