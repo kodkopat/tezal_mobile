871a2564835a54fb087ac40e3b5ba62a9ab9366c
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-20 19:10:57 +0330

    Update register_result_model.dart

--- lib/features/auth/data/models/register_result_model.dart
+++ lib/features/auth/data/models/register_result_model.dart
@@ -15,7 +15,7 @@ class RegisterResultModel {
 
   final bool success;
   final String message;
-  final String data;
+  final Data data;
 
   factory RegisterResultModel.fromRawJson(String str) =>
       RegisterResultModel.fromJson(json.decode(str));
@@ -26,12 +26,32 @@ class RegisterResultModel {
       RegisterResultModel(
         success: json["success"] == null ? null : json["success"],
         message: json["message"] == null ? null : json["message"],
-        data: json["data"] == null ? null : json["data"],
+        data: json["data"] == null ? null : Data.fromJson(json["data"]),
       );
 
   Map<String, dynamic> toJson() => {
         "success": success == null ? null : success,
         "message": message == null ? null : message,
-        "data": data == null ? null : data,
+        "data": data == null ? null : data.toJson(),
+      };
+}
+
+class Data {
+  Data({
+    @required this.userId,
+  });
+
+  final String userId;
+
+  factory Data.fromRawJson(String str) => Data.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        userId: json["userId"] == null ? null : json["userId"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId == null ? null : userId,
       };
 }
