f9b9e3eea8f944142e6346024260708b9228d580
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-20 19:03:40 +0330

    add userLocation storage key

--- lib/core/consts/consts.dart
+++ lib/core/consts/consts.dart
@@ -3,3 +3,4 @@ const String apiBaseUrl = "http://178.157.14.77/api/";
 const String storageKeyUserId = "USER_ID_STORAGE_KEY";
 const String storageKeyUserToken = "USER_TOKEN_STORAGE_KEY";
 const String storageKeyUserType = "USER_TYPE_STORAGE_KEY";
+const String storageKeyUserLocation = "USER_LOCATION_STORAGE_KEY";
