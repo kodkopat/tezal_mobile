7a0d4c39f29e0277e5be02173b051c3e382a6c81
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-23 22:49:11 +0430

    Create market_detail_product_list.dart

---
+++ lib/features/presentation/customer_pages/market_detail/widgets/market_detail_product_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../../../../data/models/market_detail_result_model.dart';
+import 'market_detail_product_list_item.dart';
+
+class MarketDetailProductList extends StatelessWidget {
+  const MarketDetailProductList({
+    Key key,
+    @required this.products,
+  }) : super(key: key);
+
+  final List<Product> products;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 248,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: products.length,
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.fromLTRB(8, 4, 8, 16),
+        itemBuilder: (context, index) {
+          return MarketDetailProductListItem(
+            product: products[index],
+            onTap: () {},
+          );
+        },
+      ),
+    );
+  }
+}
