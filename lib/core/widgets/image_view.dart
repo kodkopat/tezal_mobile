74857d23b4d8d2844f7115ab356a59c3908ea371
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-23 22:39:36 +0430

    Update image_view.dart

--- lib/core/widgets/image_view.dart
+++ lib/core/widgets/image_view.dart
@@ -1,4 +1,3 @@
-// import 'package:cached_network_image/cached_network_image.dart';
 import 'dart:convert';
 
 import 'package:division/division.dart';
@@ -7,10 +6,11 @@ import 'package:flutter/material.dart';
 import '../styles/txt_styles.dart';
 
 class ProductImageView extends StatefulWidget {
-  final List<String> images;
+  const ProductImageView({
+    @required this.images,
+  });
 
-  const ProductImageView({@required this.images})
-      : assert(images != null && images is List<String>);
+  final List<String> images;
 
   @override
   _ProductImageViewState createState() => _ProductImageViewState();
@@ -22,86 +22,72 @@ class _ProductImageViewState extends State<ProductImageView> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [
-        Container(
-          height: boxHeight,
-          child: PageView.builder(
-            pageSnapping: true,
+        Parent(
+          style: ParentStyle()
+            ..background.color(Colors.white)
+            ..borderRadius(all: 8)
+            ..boxShadow(
+              color: Colors.black.withOpacity(0.2),
+              offset: Offset(0, 4.0),
+              blur: 8,
+              spread: 0,
+            ),
+          child: SizedBox(
+            height: boxHeight,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+              child: PageView.builder(
+                pageSnapping: true,
+                itemCount: widget.images.length,
+                onPageChanged: (value) {
+                  setState(() {
+                    selectedImage = value;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return Parent(
+                    style: ParentStyle()
+                      ..width(MediaQuery.of(context).size.width)
+                      ..height(boxHeight)
+                      ..background.image(
+                        alignment: Alignment.center,
+                        path: "assets/images/placeholder.jpg",
+                        fit: BoxFit.fill,
+                      ),
+                    child: Image.memory(
+                      base64Decode(widget.images[index]),
+                      fit: BoxFit.fill,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        SizedBox(
+          height: 16,
+          child: ListView.builder(
+            shrinkWrap: true,
             itemCount: widget.images.length,
-            onPageChanged: (value) {
-              setState(() {
-                selectedImage = value;
-              });
-            },
+            scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
-              // return CachedNetworkImage(
-              //   imageUrl: "",
-              //   width: MediaQuery.of(context).size.width,
-              //   height: boxHeight,
-              //   fit: BoxFit.cover,
-              // );
-
-              // return Image.asset(
-              //   widget.images[index],
-              //   width: MediaQuery.of(context).size.width,
-              //   height: boxHeight,
-              //   fit: BoxFit.cover,
-              // );
-
-              return Parent(
-                style: ParentStyle()
-                  ..width(MediaQuery.of(context).size.width)
-                  ..height(boxHeight)
-                  ..background.image(
-                    alignment: Alignment.center,
-                    path: "assets/images/placeholder.jpg",
-                    fit: BoxFit.fill,
+              return Txt(
+                "\u2B24",
+                style: AppTxtStyles().footNote
+                  ..clone()
+                  ..padding(horizontal: 2)
+                  ..textColor(
+                    selectedImage == index ? Colors.black54 : Colors.grey[300],
                   ),
-                child: ClipRRect(
-                  // borderRadius: BorderRadius.all(
-                  //   Radius.circular(8),
-                  // ),
-                  child: Image.memory(
-                    base64Decode(widget.images[index]),
-                    fit: BoxFit.fill,
-                  ),
-                ),
               );
             },
           ),
         ),
-        Positioned(
-          bottom: 8,
-          right: 0,
-          left: 0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 16,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: widget.images.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Txt(
-                      "\u2B24",
-                      style: AppTxtStyles().footNote
-                        ..clone()
-                        ..padding(horizontal: 2)
-                        ..textColor(
-                          selectedImage == index
-                              ? Colors.black54
-                              : Colors.white,
-                        ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        )
       ],
     );
   }
