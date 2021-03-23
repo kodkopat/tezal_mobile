b8f03af88de9466225c8355eae762f5d7a3670ac
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-23 22:48:03 +0430

    Create adderss_list_item.dart

---
+++ lib/features/presentation/customer_pages/addresses/widgets/adderss_list_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddressListItem extends StatelessWidget {
+  const AddressListItem({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
