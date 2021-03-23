376f7f01d991bdbbf4daf758fcc3015d25e33377
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-23 22:48:22 +0430

    Create dashboard_page.dart

---
+++ lib/features/presentation/customer_pages/dashboard/dashboard_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../home/home_page.dart';
+import 'widgets/bottom_app_bar.dart';
+
+class CustomerDashBoardPage extends StatefulWidget {
+  static const route = "/customer_dashboard";
+
+  @override
+  _CustomerDashBoardPageState createState() => _CustomerDashBoardPageState();
+}
+
+class _CustomerDashBoardPageState extends State<CustomerDashBoardPage> {
+  Widget currentWidget;
+  CustomBottomAppBar bottomAppBar;
+
+  void initializeState() {
+    bottomAppBar = CustomBottomAppBar(
+      onIndexChanged: (widget) {
+        setState(() => currentWidget = widget);
+      },
+    );
+    // must be access this widget from CustomBottomAppBar
+    currentWidget = HomePage();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: currentWidget,
+      bottomNavigationBar: bottomAppBar,
+    );
+  }
+}
