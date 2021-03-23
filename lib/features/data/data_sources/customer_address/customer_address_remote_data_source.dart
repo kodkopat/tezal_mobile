67b46f5ce64f9bce93f8a8ff7656f05e1e71cc86
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-23 22:45:52 +0430

    Create customer_address_remote_data_source.dart

---
+++ lib/features/data/data_sources/customer_address/customer_address_remote_data_source.dart
@@ -0,0 +1,67 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/retrofit.dart';
+
+import '../../../../core/consts/consts.dart';
+import '../../models/address_actions_result_model.dart';
+import '../../models/address_result_model.dart';
+import '../../models/addresses_result_model.dart';
+import '../../models/cities_result_model.dart';
+import '../../models/provinces_result_model.dart';
+
+part 'customer_address_remote_data_source.g.dart';
+
+@RestApi(baseUrl: apiBaseUrl)
+abstract class CustomerAddressRemoteDataSource {
+  factory CustomerAddressRemoteDataSource(Dio dio, {String baseUrl}) =
+      _CustomerAddressRemoteDataSource;
+
+  static const _apiUrlPrefix = "customer/Address";
+
+  @GET("$_apiUrlPrefix/GetProvince")
+  @Headers({"Content-Type": "application/json", "Accept": "text/plain"})
+  Future<ProvincesResultModel> getProvince();
+
+  @GET("$_apiUrlPrefix/GetCity")
+  @Headers({"Content-Type": "application/json", "Accept": "text/plain"})
+  Future<CitiesResultModel> getCity(
+    @Query("provinceId") String provinceId,
+  );
+
+  @GET("$_apiUrlPrefix/save")
+  @Headers({"Content-Type": "application/json", "Accept": "text/plain"})
+  Future<AddressActionsResultModel> save(
+    @Header("token") String token,
+    @Field() String address,
+    @Field() String description,
+    @Field() bool isDefault,
+    @Field() String cityId,
+    @Field() String name,
+  );
+
+  @GET("$_apiUrlPrefix/SetDefaultAddress")
+  @Headers({"Content-Type": "application/json", "Accept": "text/plain"})
+  Future<AddressActionsResultModel> setdefaultAddress(
+    @Header("token") String token,
+    @Query("Id") String id,
+  );
+
+  @GET("$_apiUrlPrefix/RemoveAddress")
+  @Headers({"Content-Type": "application/json", "Accept": "text/plain"})
+  Future<AddressActionsResultModel> removeAddress(
+    @Header("token") String token,
+    @Query("Id") String id,
+  );
+
+  @GET("$_apiUrlPrefix/getAddress")
+  @Headers({"Content-Type": "application/json", "Accept": "text/plain"})
+  Future<AddressResultModel> getAddress(
+    @Header("token") String token,
+    @Query("Id") String id,
+  );
+
+  @GET("$_apiUrlPrefix/getAddressess")
+  @Headers({"Content-Type": "application/json", "Accept": "text/plain"})
+  Future<AddressesResultModel> getAddresses(
+    @Header("token") String token,
+  );
+}
