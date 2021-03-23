d5f5e97368908bb010c60cb3bd8f87512c5375bd
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-23 22:49:44 +0430

    Create profile_menu.dart

---
+++ lib/features/presentation/customer_pages/profile/widgets/profile_menu.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:sailor/sailor.dart';
+
+import '../../../../../core/page_routes/routes.dart';
+import '../../addresses/addresses_page.dart';
+import 'modal_about_us.dart';
+import 'modal_log_out.dart';
+import 'profile_menu_item.dart';
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      children: _ProfileMenu.items(context),
+    );
+  }
+}
+
+class _ProfileMenu {
+  static var _divider = Divider(
+    color: Colors.black12,
+    thickness: 0.5,
+    height: 0,
+    indent: 56,
+  );
+
+  static List<Widget> items(BuildContext context) => [
+        ProfileMenuItem(
+          text: "اطلاعات شخصی",
+          iconData: Feather.user,
+          onTap: () {},
+        ),
+        ProfileMenuItem(
+          text: "آدرس‌های من",
+          iconData: Feather.map_pin,
+          onTap: () {
+            Routes.sailor(AddressesPage.route);
+          },
+        ),
+        ProfileMenuItem(
+          text: "سفارشات قبلی",
+          iconData: Feather.shopping_bag,
+          onTap: () {},
+        ),
+        _divider,
+        ProfileMenuItem(
+          text: "پشتیبانی",
+          iconData: Feather.life_buoy,
+          onTap: () {},
+        ),
+        ProfileMenuItem(
+          text: "قوانین و مقررات",
+          iconData: Feather.file_text,
+          onTap: () {},
+        ),
+        ProfileMenuItem(
+          text: "درباره ما",
+          iconData: Feather.info,
+          onTap: () {
+            showDialog(
+              context: context,
+              useSafeArea: true,
+              barrierDismissible: true,
+              barrierColor: Colors.black.withOpacity(0.2),
+              builder: (context) {
+                return AboutUsModal();
+              },
+            );
+          },
+        ),
+        _divider,
+        ProfileMenuItem(
+          text: "تنظیمات",
+          iconData: Feather.settings,
+          onTap: () {},
+        ),
+        ProfileMenuItem(
+          text: "خروج از حساب کاربری",
+          iconData: Feather.log_out,
+          showChevron: false,
+          onTap: () {
+            showDialog(
+              context: context,
+              useSafeArea: true,
+              barrierDismissible: true,
+              barrierColor: Colors.black.withOpacity(0.2),
+              builder: (context) {
+                return LogOutModal(
+                  onAccept: () async {
+                    var secureStorage = FlutterSecureStorage();
+                    await secureStorage.deleteAll();
+                    Routes.sailor.pop();
+                    Routes.sailor.navigate(
+                      "/",
+                      navigationType: NavigationType.pushReplace,
+                    );
+                  },
+                  onDiscard: () {
+                    Routes.sailor.pop();
+                  },
+                );
+              },
+            );
+          },
+        ),
+      ];
+}
