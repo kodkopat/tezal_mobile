bdc636d5fd506f47c8edb52a57cd544d49152de4
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-24 19:24:31 +0430

    Create customer_search_repository.dart

---
+++ lib/features/data/repositories/customer_search_repository.dart
@@ -0,0 +1,73 @@
+import 'package:dartz/dartz.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import '../../../core/exceptions/api_failure.dart';
+import '../../../core/exceptions/connection_failure.dart';
+import '../../../core/exceptions/failure.dart';
+import '../../../core/services/location.dart';
+import '../data_sources/customer_search/customer_search_local_data_source.dart';
+import '../data_sources/customer_search/customer_search_remote_data_source.dart';
+import '../models/clear_search_terms_result_model.dart';
+import '../models/search_result_model.dart';
+import '../models/search_terms_result_model.dart';
+
+class CustomerSearchRepository {
+  CustomerSearchRepository()
+      : _connectionChecker = DataConnectionChecker(),
+        _remoteDataSource = CustomerSearchRemoteDataSource(Dio()),
+        _localDataSource = CustomerSearchLocalDataSource();
+
+  final String connectionFailedMsg = "دسترسی به اینترنت امکان‌پذیر نمی‌باشد!";
+  final DataConnectionChecker _connectionChecker;
+  final CustomerSearchRemoteDataSource _remoteDataSource;
+  // ignore: unused_field
+  final CustomerSearchLocalDataSource _localDataSource;
+
+  Future<Either<Failure, SearchResultModel>> search(
+    BuildContext context, {
+    @required String term,
+  }) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      var lang = Localizations.localeOf(context).languageCode;
+      var position = await LocationService.getSavedLocation();
+
+      var result = await _remoteDataSource.search(
+        lang,
+        "${position.latitude}",
+        "${position.longitude}",
+        term,
+      );
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+
+  Future<Either<Failure, SearchTermsResultModel>> searchTerms({
+    @required String term,
+  }) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      var result = await _remoteDataSource.getSearchTerms();
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+
+  Future<Either<Failure, ClearSearchTermsResultModel>> clearSearchTerms({
+    @required String term,
+  }) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      var result = await _remoteDataSource.clearSearchTerms();
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+}
