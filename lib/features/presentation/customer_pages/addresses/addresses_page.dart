f98f24f299a237f140eb9cf07c9573c5ae7b15f6
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-24 19:24:36 +0430

    Update addresses_page.dart

--- lib/features/presentation/customer_pages/addresses/addresses_page.dart
+++ lib/features/presentation/customer_pages/addresses/addresses_page.dart
@@ -1,4 +1,4 @@
-import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart' hide State;
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
@@ -9,15 +9,19 @@ import '../../../../core/themes/app_theme.dart';
 import '../../../../core/widgets/custom_future_builder.dart';
 import '../../../../core/widgets/loading.dart';
 import '../../../../core/widgets/simple_app_bar.dart';
+import '../../../data/models/addresses_result_model.dart';
 import '../../../data/repositories/customer_address_repository.dart';
 import 'widgets/address_list.dart';
 import 'widgets/modal_save_address.dart';
 
-class AddressesPage extends StatelessWidget {
+class AddressesPage extends StatefulWidget {
   static const route = "/customer_addresses";
 
-  AddressesPage({Key key}) : super(key: key);
+  @override
+  _AddressesPageState createState() => _AddressesPageState();
+}
 
+class _AddressesPageState extends State<AddressesPage> {
   final _customerAddressRepo = CustomerAddressRepository();
 
   @override
@@ -27,48 +31,59 @@ class AddressesPage extends StatelessWidget {
         text: "آدرس‌های من",
         showBackBtn: true,
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
+      body: futureBuilder,
+      floatingActionButton: fab,
+    );
+  }
+
+  Widget get futureBuilder {
+    return CustomFutureBuilder<Either<Failure, AddressesResultModel>>(
+      future: _customerAddressRepo.addresses,
+      successBuilder: (context, data) {
+        return data.fold(
+          (l) => Txt(
+            l.message,
+            style: AppTxtStyles().body..alignment.center(),
+          ),
+          (r) => AddressList(
+            addresses: r.data,
+            customerAddressRepo: _customerAddressRepo,
+            onSetAddressDefaultComplete: () => setState(() {}),
+            onShowAddressDetailComplete: () => setState(() {}),
+            onEditAddressComplete: () => setState(() {}),
+            onRemoveAddressComplete: () => setState(() {}),
+          ),
+        );
+      },
+      errorBuilder: (context, data) {
+        return AppLoading(color: AppTheme.customerPrimary);
+      },
+    );
+  }
+
+  // floating action button
+  Widget get fab {
+    return FloatingActionButton(
+      onPressed: fabOnTap,
+      child: Icon(
+        Feather.plus,
         color: Colors.white,
-      )
-      /*CustomFutureBuilder<Either<Failure, dynamic>>(
-        // future: _customerAddressRepo,
-        future: Future.value(),
-        successBuilder: (context, data) {
-          return data.fold(
-            (l) => Txt(
-              l.message,
-              style: AppTxtStyles().body..alignment.center(),
-            ),
-            (r) => AddressList(),
-          );
-        },
-        errorBuilder: (context, data) {
-          return AppLoading(color: AppTheme.customerPrimary);
-        },
-      ) */
-      ,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            useSafeArea: true,
-            barrierDismissible: true,
-            barrierColor: Colors.black.withOpacity(0.2),
-            builder: (context) {
-              return SaveAddressModal(
-                customerAddressRepo: _customerAddressRepo,
-              );
-            },
-          );
-        },
-        child: Icon(
-          Feather.plus,
-          color: Colors.white,
-          size: 24,
-        ),
+        size: 24,
       ),
     );
+  }
+
+  void fabOnTap() {
+    showDialog(
+      context: context,
+      useSafeArea: true,
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.2),
+      builder: (context) {
+        return SaveAddressModal(
+          customerAddressRepo: _customerAddressRepo,
+        );
+      },
+    ).then((value) => setState(() {}));
   }
 }
