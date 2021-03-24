776d9387e8c0859b55ba0fb18027ed316890da44
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-24 19:23:45 +0430

    Update search_page.dart

--- lib/features/presentation/customer_pages/search/search_page.dart
+++ lib/features/presentation/customer_pages/search/search_page.dart
@@ -1,8 +1,13 @@
-import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/themes/app_theme.dart';
+import '../../../../core/widgets/loading.dart';
 import '../../../../core/widgets/simple_app_bar.dart';
+import '../../../data/models/search_result_model.dart';
+import '../../../data/repositories/customer_market_repository.dart';
+import '../../../data/repositories/customer_search_repository.dart';
+import 'widgets/search_box.dart';
+import 'widgets/search_result_list.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key key}) : super(key: key);
@@ -12,18 +17,71 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final searchRepo = CustomerSearchRepository();
+  final marketRepo = CustomerMarketRepository();
+
+  final searchCtrl = TextEditingController();
+  var onSearchTap;
+
+  List<Market> markets = [];
+
+  bool loading = false;
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
         appBar: SimpleAppBar.intance(text: "جستجو"),
-        body: Center(
-          child: Txt(
-            "جستجو",
-            style: AppTxtStyles().body,
-          ),
-        ),
+        body: loading
+            ? AppLoading(color: AppTheme.customerPrimary)
+            : Column(
+                children: [
+                  SearchBox(
+                    controller: searchCtrl,
+                    onSearchTap: () async {
+                      if (searchCtrl.text.trim().isEmpty) {
+                        return;
+                      }
+
+                      var result = await searchRepo.search(
+                        context,
+                        term: searchCtrl.text,
+                      );
+
+                      result.fold(
+                        (l) => null,
+                        (r) {
+                          print("r: $r\n");
+
+                          if (markets != null) {
+                            markets.clear();
+                            markets.addAll(r.data.markets);
+                          } else {
+                            markets = r.data.markets;
+                          }
+
+                          setState(() {});
+                        },
+                      );
+                    },
+                  ),
+                  Divider(
+                    color: Colors.black12,
+                    thickness: 0.5,
+                    height: 0,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: SearchResultList(
+                        repository: marketRepo,
+                        markets: markets,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
