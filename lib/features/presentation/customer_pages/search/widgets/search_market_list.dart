8299a099e85bd62c56b04a8de9327e1e45915dd1
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-27 18:23:30 +0430

    Create search_market_list.dart

---
+++ lib/features/presentation/customer_pages/search/widgets/search_market_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../../../data/models/search_result_model.dart';
+import 'search_market_list_item.dart';
+
+class SearchMarketList extends StatelessWidget {
+  const SearchMarketList({
+    Key key,
+    @required this.markets,
+  }) : super(key: key);
+
+  final List<Market> markets;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: markets.length,
+      scrollDirection: Axis.vertical,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return SearchMarketListItem(
+          market: markets[index],
+        );
+      },
+    );
+  }
+}
