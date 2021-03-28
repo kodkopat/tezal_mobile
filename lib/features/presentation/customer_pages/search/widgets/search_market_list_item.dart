7329a563b5abb19f5c16d2acbbf77b5a6e5f41be
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-28 19:03:41 +0430

    Update search_market_list_item.dart

--- lib/features/presentation/customer_pages/search/widgets/search_market_list_item.dart
+++ lib/features/presentation/customer_pages/search/widgets/search_market_list_item.dart
@@ -1,8 +1,10 @@
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/page_routes/routes.dart';
 import '../../../../../core/styles/txt_styles.dart';
 import '../../../../data/models/search_result_model.dart';
+import '../../market_detail/market_detail_page.dart';
 import 'search_market_product_list.dart';
 
 class SearchMarketListItem extends StatelessWidget {
@@ -29,8 +31,21 @@ class SearchMarketListItem extends StatelessWidget {
                 style: AppTxtStyles().body..bold(),
               ),
               Txt(
-                "مشاهده همه محصولات \u203A",
-                style: AppTxtStyles().body,
+                "جزئیات فروشگاه \u00BB",
+                gesture: Gestures()
+                  ..onTap(() {
+                    Routes.sailor.navigate(
+                      MarketDetailPage.route,
+                      params: {
+                        "marketId": market,
+                      },
+                    );
+                  }),
+                style: AppTxtStyles().footNote
+                  ..borderRadius(all: 4)
+                  ..margin(top: 2)
+                  ..padding(horizontal: 4, vertical: 2)
+                  ..ripple(true),
               ),
             ],
           ),
