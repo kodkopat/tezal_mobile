768866baa7b8bca9ca754e49f07e6520a9e71b67
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-28 19:01:07 +0430

    Update customer_market_repository.dart

--- lib/features/data/repositories/customer_market_repository.dart
+++ lib/features/data/repositories/customer_market_repository.dart
@@ -10,22 +10,25 @@ import '../../../core/exceptions/failure.dart';
 import '../../../core/services/location.dart';
 import '../../data/data_sources/customer_market/customer_market_local_data_source.dart';
 import '../../data/data_sources/customer_market/customer_market_remote_data_source.dart';
+import '../models/market_comments_result_model.dart';
 import '../models/market_detail_result_model.dart';
 import '../models/nearby_markets_result_model.dart';
-import '../models/photo_result_model.dart';
 import '../models/photos_result_model.dart';
+import 'auth_repository.dart';
 
 class CustomerMarketRepository {
   CustomerMarketRepository()
       : _connectionChecker = DataConnectionChecker(),
         _remoteDataSource = CustomerMarketRemoteDataSource(Dio()),
-        _localDataSource = CustomerMarketLocalDataSource();
+        _localDataSource = CustomerMarketLocalDataSource(),
+        _authRepo = AuthRepository();
 
   final String connectionFailedMsg = "دسترسی به اینترنت امکان‌پذیر نمی‌باشد!";
   final DataConnectionChecker _connectionChecker;
   final CustomerMarketRemoteDataSource _remoteDataSource;
   // ignore: unused_field
   final CustomerMarketLocalDataSource _localDataSource;
+  final AuthRepository _authRepo;
 
   Future<Either<Failure, NearByMarketsResultModel>> nearByMarkets(
     BuildContext context, {
@@ -56,13 +59,14 @@ class CustomerMarketRepository {
     if (!await _connectionChecker.hasConnection) {
       return Left(ConnectionFailure(connectionFailedMsg));
     } else {
-      var result = await _remoteDataSource.getMarketDetail(marketId);
+      final userToken = await _authRepo.userToken;
+      var result = await _remoteDataSource.getMarketDetail(userToken, marketId);
 
       return result.success ? Right(result) : Left(ApiFailure(result.message));
     }
   }
 
-  Future<Either<Failure, PhotoResultModel>> photo({
+  Future<Either<Failure, PhotosResultModel>> photo({
     @required String marketId,
   }) async {
     if (!await _connectionChecker.hasConnection) {
@@ -83,6 +87,37 @@ class CustomerMarketRepository {
       var result = await _remoteDataSource.getPhotos(marketId);
 
       return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+
+  Future<Either<Failure, MarketCommentsResultModel>> marketComments({
+    @required String marketId,
+    @required int page,
+    // @required String orderBy,
+    // @required String marketId,
+  }) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      var result = await _remoteDataSource.getListComment(marketId, page);
+
+      return result != null
+          ? Right(result)
+          : Left(ApiFailure("failed to load comments"));
+    }
+  }
+
+  Future<Either<Failure, dynamic>> addComment({
+    @required String comment,
+    @required int point,
+    @required String marketId,
+  }) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      var result = await _remoteDataSource.addComment(comment, point, marketId);
+
+      return result != null ? Right(result) : Left(ApiFailure("failed to add"));
     }
   }
 }
