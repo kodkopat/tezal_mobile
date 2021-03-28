736dd7228e1b2f5e4f0cb81de5b5f5429cf2ad6f
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-28 19:01:46 +0430

    Create address_detail_actions_menu_item.dart

---
+++ lib/features/presentation/customer_pages/address_detail/widgets/address_detail_actions_menu_item.dart
@@ -0,0 +1,45 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/styles/txt_styles.dart';
+
+class AddressDetailActionsMenuItem extends StatelessWidget {
+  const AddressDetailActionsMenuItem({
+    Key key,
+    @required this.text,
+    @required this.iconData,
+    @required this.onTap,
+    this.color,
+  }) : super(key: key);
+
+  final String text;
+  final IconData iconData;
+  final void Function() onTap;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Parent(
+      gesture: Gestures()..onTap(onTap),
+      style: ParentStyle()
+        ..padding(horizontal: 16, vertical: 8)
+        ..ripple(true),
+      child: Row(
+        textDirection: TextDirection.rtl,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(
+            iconData,
+            color: color ?? Colors.black,
+            size: 18,
+          ),
+          SizedBox(width: 8),
+          Txt(
+            text,
+            style: AppTxtStyles().body..textColor(color ?? Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
