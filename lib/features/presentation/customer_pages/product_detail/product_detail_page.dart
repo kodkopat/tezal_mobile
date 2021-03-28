fba53d8969ed1a0349dad43acd8ef8e7b5b000d2
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-28 19:03:33 +0430

    Create product_detail_page.dart

---
+++ lib/features/presentation/customer_pages/product_detail/product_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  static const route = "/customer_product_detail";
+
+  const ProductDetailPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
