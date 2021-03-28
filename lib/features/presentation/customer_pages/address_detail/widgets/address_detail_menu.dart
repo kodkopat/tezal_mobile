1aa7c58c6470e43ada92637094d226169197e26a
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-28 19:01:50 +0430

    Create address_detail_menu.dart

---
+++ lib/features/presentation/customer_pages/address_detail/widgets/address_detail_menu.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../../data/models/address_result_model.dart';
+import 'address_detail_menu_item.dart';
+
+class AddressDetailMenu extends StatelessWidget {
+  const AddressDetailMenu({
+    Key key,
+    @required this.address,
+  }) : super(key: key);
+
+  final Address address;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        textDirection: TextDirection.rtl,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AddressDetailMenuItem(
+            title: "عنوان:",
+            text: "${address.name}",
+          ),
+          AddressDetailMenuItem(
+            title: "آدرس:",
+            text: "${address.address}",
+          ),
+          AddressDetailMenuItem(
+            title: "توضیحات:",
+            text: "${address.description}",
+          ),
+          AddressDetailMenuItem(
+            title: "استان:",
+            text: "${address.province}",
+          ),
+          AddressDetailMenuItem(
+            title: "شهر:",
+            text: "${address.city}",
+          ),
+        ],
+      ),
+    );
+  }
+}
