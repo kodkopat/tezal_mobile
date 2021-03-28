ddeb2bf0a3090e03d08cbe5bb44e62e7b17707fd
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-28 19:02:07 +0430

    Update addresses_page.dart

--- lib/features/presentation/customer_pages/addresses/addresses_page.dart
+++ lib/features/presentation/customer_pages/addresses/addresses_page.dart
@@ -12,7 +12,7 @@ import '../../../../core/widgets/simple_app_bar.dart';
 import '../../../data/models/addresses_result_model.dart';
 import '../../../data/repositories/customer_address_repository.dart';
 import 'widgets/address_list.dart';
-import 'widgets/modal_save_address.dart';
+import '../address_detail/widgets/modal_save_address.dart';
 
 class AddressesPage extends StatefulWidget {
   static const route = "/customer_addresses";
