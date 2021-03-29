9d416dd4c81755493aff0439e22962b1db898905
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-29 17:15:04 +0430

    Create search_notifier.dart

---
+++ lib/features/presentation/providers/customer_providers/search_notifier.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/repositories/customer_search_repository.dart';
+
+class SearchNotifier extends ChangeNotifier {
+  SearchNotifier(
+    this.context, {
+    @required this.customerSearchRepo,
+  });
+
+  final BuildContext context;
+  final CustomerSearchRepository customerSearchRepo;
+
+  bool loading = true;
+
+  List<String> _searchTerms = [];
+
+  List<String> get searchTerms => _searchTerms;
+
+  void fetchSearchTerms() async {
+    var result = await customerSearchRepo.searchTerms(context);
+
+    result.fold(
+      (left) {
+        return null;
+      },
+      (right) {
+        _searchTerms.addAll(right.data);
+      },
+    );
+
+    loading = false;
+
+    notifyListeners();
+  }
+}
