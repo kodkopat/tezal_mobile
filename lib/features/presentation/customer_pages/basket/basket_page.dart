c88996170b959abbf4b6f949157217669f940ade
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-30 18:42:42 +0430

    Update basket_page.dart

--- lib/features/presentation/customer_pages/basket/basket_page.dart
+++ lib/features/presentation/customer_pages/basket/basket_page.dart
@@ -1,48 +1,60 @@
-import 'package:dartz/dartz.dart' hide State;
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../../../../core/exceptions/failure.dart';
 import '../../../../core/styles/txt_styles.dart';
 import '../../../../core/themes/app_theme.dart';
-import '../../../../core/widgets/custom_future_builder.dart';
 import '../../../../core/widgets/loading.dart';
 import '../../../../core/widgets/simple_app_bar.dart';
-import '../../../data/models/basket_result_model.dart';
-import '../../../data/repositories/customer_basket_repository.dart';
+import '../../providers/customer_providers/basket_provider.dart';
+import 'widgets/basket_actions_box.dart';
 import 'widgets/basket_list.dart';
 
-class BasketPage extends StatefulWidget {
-  const BasketPage({Key key}) : super(key: key);
-
-  @override
-  _BasketPageState createState() => _BasketPageState();
-}
-
-class _BasketPageState extends State<BasketPage> {
-  final customerBasketRepo = CustomerBasketRepository();
-
+class BasketPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var consumer = Consumer<BasketNotifier>(
+      builder: (context, provider, child) {
+        if (provider.basketResultModel == null && provider.errorMsg == null) {
+          provider.fetchBasket();
+        }
+
+        return provider.loading
+            ? AppLoading(color: AppTheme.customerPrimary)
+            : provider.errorMsg != null
+                ? Txt(
+                    provider.errorMsg,
+                    style: AppTxtStyles().body..alignment.center(),
+                  )
+                : provider.basketResultModel.data == null
+                    ? Txt(
+                        "سبد خرید محصولات شما خالی است",
+                        style: AppTxtStyles().body..alignment.center(),
+                      )
+                    : SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            BasketList(
+                              basketItems:
+                                  provider.basketResultModel.data?.items,
+                              basketNotifier: provider,
+                            ),
+                            SizedBox(height: 8),
+                            BasketActionsBox(
+                              basket: provider.basketResultModel,
+                              basketNotifier: provider,
+                            ),
+                          ],
+                        ),
+                      );
+      },
+    );
+
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
         appBar: SimpleAppBar.intance(text: "سبد خرید"),
-        body: CustomFutureBuilder<Either<Failure, BasketResultModel>>(
-          future: customerBasketRepo.basket,
-          successBuilder: (context, data) {
-            return data.fold(
-              (l) => Txt(
-                l.message,
-                style: AppTxtStyles().body..alignment.center(),
-              ),
-              (r) => BasketList(basketItems: r.data.items),
-            );
-          },
-          errorBuilder: (context, data) {
-            return AppLoading(color: AppTheme.customerPrimary);
-          },
-        ),
+        body: consumer,
       ),
     );
   }
