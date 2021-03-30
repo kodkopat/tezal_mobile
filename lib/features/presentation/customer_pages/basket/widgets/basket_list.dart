24b3c06d5ead7e56bfbba1bb27e170228600c3b0
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-30 18:41:36 +0430

    edit basket list

--- lib/features/presentation/customer_pages/basket/widgets/basket_list.dart
+++ lib/features/presentation/customer_pages/basket/widgets/basket_list.dart
@@ -1,26 +1,60 @@
+import 'package:division/division.dart';
 import 'package:flutter/material.dart';
-import 'package:tezal/features/data/models/basket_result_model.dart';
 
+import '../../../../data/models/basket_result_model.dart';
+import '../../../providers/customer_providers/basket_provider.dart';
 import 'basket_list_item.dart';
 
 class BasketList extends StatelessWidget {
   const BasketList({
     Key key,
     @required this.basketItems,
+    @required this.basketNotifier,
   }) : super(key: key);
 
   final List<BasketItem> basketItems;
+  final BasketNotifier basketNotifier;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: basketItems.length,
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      itemBuilder: (context, index) {
-        return BasketListItem(
-          baskteItem: basketItems[index],
-        );
-      },
+    return Parent(
+      style: ParentStyle()
+        ..margin(vertical: 8)
+        ..margin(horizontal: 16, vertical: 8)
+        ..background.color(Colors.white)
+        ..borderRadius(all: 8)
+        ..boxShadow(
+          color: Colors.black.withOpacity(0.2),
+          offset: Offset(0, 4.0),
+          blur: 8,
+          spread: 0,
+        ),
+      child: ListView.separated(
+        shrinkWrap: true,
+        itemCount: basketItems.length,
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return BasketListItem(
+            basketItem: basketItems[index],
+            basketNotifier: basketNotifier,
+            onRemoveItem: () async {
+              await basketNotifier.customerBasketRepo.removeProductToBasket(
+                productId: basketItems[index].id,
+                amount: basketItems[index].amount,
+              );
+              basketNotifier.refresh();
+            },
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(
+            color: Colors.black12,
+            thickness: 0.5,
+            height: 32,
+          );
+        },
+      ),
     );
   }
 }
--- lib/features/presentation/customer_pages/basket/widgets/basket_list_item.dart
+++ lib/features/presentation/customer_pages/basket/widgets/basket_list_item.dart
@@ -1,23 +1,251 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
-import 'package:tezal/core/styles/txt_styles.dart';
-import 'package:tezal/features/data/models/basket_result_model.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:intl/intl.dart' as intl;
+
+import '../../../../../core/exceptions/failure.dart';
+import '../../../../../core/styles/txt_styles.dart';
+import '../../../../../core/widgets/custom_future_builder.dart';
+import '../../../../data/models/basket_result_model.dart';
+import '../../../../data/models/photos_result_model.dart';
+import '../../../customer_widgets/custom_rich_text.dart';
+import '../../../customer_widgets/product_list/product_list_item_counter.dart';
+import '../../../providers/customer_providers/basket_provider.dart';
 
 class BasketListItem extends StatelessWidget {
-  const BasketListItem({
+  BasketListItem({
     Key key,
-    this.baskteItem,
+    @required this.basketItem,
+    @required this.onRemoveItem,
+    @required this.basketNotifier,
   }) : super(key: key);
 
-  final BasketItem baskteItem;
+  final BasketItem basketItem;
+  final void Function() onRemoveItem;
+  final BasketNotifier basketNotifier;
+  final productCounterKey = GlobalKey<ProductListItemCounterState>();
 
   @override
   Widget build(BuildContext context) {
-    return Txt(
-      "${baskteItem.productName} ${baskteItem.amount}",
-      style: AppTxtStyles().body
-        ..padding(horizontal: 16, vertical: 8)
-        ..alignment.centerRight(),
+    return Column(
+      textDirection: TextDirection.rtl,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          textDirection: TextDirection.rtl,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Parent(
+              style: ParentStyle()
+                ..width(96)
+                ..height(96)
+                ..borderRadius(all: 8)
+                ..background.image(
+                  alignment: Alignment.center,
+                  path: "assets/images/placeholder.jpg",
+                  fit: BoxFit.fill,
+                ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(8),
+                ),
+                child: _futureImgFile,
+              ),
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: Column(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    textDirection: TextDirection.rtl,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Txt(
+                        "${basketItem.productName}",
+                        style: AppTxtStyles().subHeading..bold(),
+                      ),
+                      Parent(
+                        gesture: Gestures()..onTap(onRemoveItem),
+                        style: ParentStyle()
+                          ..width(36)
+                          ..height(36)
+                          ..alignment.centerLeft()
+                          ..borderRadius(all: 18)
+                          ..ripple(true),
+                        child: Icon(
+                          Feather.trash_2,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 4),
+                  Row(
+                    textDirection: TextDirection.rtl,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _fieldOriginalPrice(),
+                      _fieldDiscountedRate(),
+                    ],
+                  ),
+                  _fieldDiscountPrice(),
+                  _fieldDiscountedPrice(),
+                ],
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 16),
+        ProductListItemCounter(
+          key: productCounterKey,
+          defaultValue: basketItem.amount,
+          hieght: 32,
+          onIncrease: (value) async {
+            await basketNotifier.customerBasketRepo.addProductToBasket(
+              productId: basketItem.id,
+              amount: 1,
+            );
+            basketNotifier.refresh();
+          },
+          onDecrease: (value) async {
+            await basketNotifier.customerBasketRepo.removeProductToBasket(
+              productId: basketItem.id,
+              amount: 1,
+            );
+            basketNotifier.refresh();
+          },
+        ),
+      ],
     );
+  }
+
+  Widget get _futureImgFile {
+    return CustomFutureBuilder<Either<Failure, PhotosResultModel>>(
+      future: basketNotifier.customerProductRepo.productphoto(
+        id: basketItem.id,
+      ),
+      successBuilder: (context, data) {
+        return data.fold(
+          (l) => SizedBox(),
+          (r) => Image.memory(
+            base64Decode(r.data.photos.first),
+          ),
+        );
+      },
+      errorBuilder: (context, data) => SizedBox(),
+    );
+  }
+
+  Widget _fieldOriginalPrice() {
+    return CustomRichText(
+      title: "قیمت: ",
+      text: _generateOriginalPrice(),
+      dashedLineText: true,
+    );
+  }
+
+  Widget _fieldDiscountPrice() {
+    return CustomRichText(
+      title: "تخفیف:",
+      text: _generateDiscountPrice(),
+    );
+  }
+
+  Widget _fieldDiscountedPrice() {
+    return CustomRichText(
+      title: "قیمت نهایی:",
+      text: _generateDiscountedPrice(),
+    );
+  }
+
+  Widget _fieldDiscountedRate() {
+    if (_generateDiscountedRate().isEmpty) {
+      return SizedBox();
+    }
+
+    return Txt(
+      _generateDiscountedRate(),
+      style: AppTxtStyles().footNote
+        ..width(36)
+        ..fontSize(10)
+        ..textDirection(TextDirection.ltr)
+        ..textColor(Colors.white)
+        ..background.color(Colors.red)
+        ..borderRadius(all: 4),
+    );
+  }
+
+  String _generateOriginalPrice() {
+    var priceTxt;
+    if (basketItem.originalPrice == null) {
+      priceTxt = " ذکر نشده ";
+    } else if (basketItem.originalPrice == 0) {
+      priceTxt = " رایگان ";
+    } else {
+      var temp;
+      if ("${basketItem.originalPrice}".length >= 3) {
+        temp = intl.NumberFormat("#,000").format(basketItem.originalPrice);
+      } else {
+        temp = "${basketItem.originalPrice}";
+      }
+
+      priceTxt = " $temp " + "تومان";
+    }
+
+    return priceTxt;
+  }
+
+  String _generateDiscountPrice() {
+    var priceTxt;
+    if (basketItem.discount == null) {
+      priceTxt = " ذکر نشده ";
+    } else {
+      var temp;
+      if ("${basketItem.discount}".length >= 3) {
+        temp = intl.NumberFormat("#,000").format(basketItem.discount);
+      } else {
+        temp = "${basketItem.discount}";
+      }
+
+      priceTxt = " $temp " + "تومان";
+    }
+
+    return priceTxt;
+  }
+
+  String _generateDiscountedPrice() {
+    var priceTxt;
+    if (basketItem.discountedPrice == null) {
+      priceTxt = " ذکر نشده ";
+    } else if (basketItem.discountedPrice == 0) {
+      priceTxt = " رایگان ";
+    } else {
+      var temp;
+      if ("${basketItem.discountedPrice}".length >= 3) {
+        temp = intl.NumberFormat("#,000").format(basketItem.discountedPrice);
+      } else {
+        temp = "${basketItem.discountedPrice}";
+      }
+
+      priceTxt = " $temp " + "تومان";
+    }
+
+    return priceTxt;
+  }
+
+  String _generateDiscountedRate() {
+    double discountRateTxt =
+        100 - (basketItem.discountedPrice) * 100 / (basketItem.originalPrice);
+    if (discountRateTxt < 1)
+      return "0٪";
+    else
+      return "${discountRateTxt.toStringAsFixed(1)}٪";
   }
 }
