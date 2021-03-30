d780be79a80589e6d61ddb8ed989cc9c2d8d5c14
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-03-30 18:42:06 +0430

    Create custom_rich_text.dart

---
+++ lib/features/presentation/customer_widgets/custom_rich_text.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomRichText extends StatelessWidget {
+  CustomRichText({
+    @required this.title,
+    @required this.text,
+    this.textAlign,
+    this.dashedLineText,
+  });
+
+  final String title;
+  final String text;
+  final TextAlign textAlign;
+  final bool dashedLineText;
+
+  final _textStyle = TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.5,
+    fontFamily: 'Yekan',
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textDirection: TextDirection.rtl,
+      textAlign: textAlign ?? TextAlign.right,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: title,
+            style: _textStyle,
+          ),
+          TextSpan(
+            text: text,
+            style: _textStyle.copyWith(
+              decoration: dashedLineText ?? false
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none,
+              fontWeight: FontWeight.w400,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
