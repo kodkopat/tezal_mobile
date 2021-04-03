cb4290c97a6e76b83b4932328546beb7020d7985
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-03 20:52:42 +0430

    rewrite addresses page with provider

--- lib/features/presentation/customer_pages/addresses/addresses_page.dart
+++ lib/features/presentation/customer_pages/addresses/addresses_page.dart
@@ -1,86 +1,65 @@
-import 'package:dartz/dartz.dart' hide State;
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:provider/provider.dart';
 
-import '../../../../core/exceptions/failure.dart';
+import '../../../../core/page_routes/routes.dart';
 import '../../../../core/styles/txt_styles.dart';
 import '../../../../core/themes/app_theme.dart';
-import '../../../../core/widgets/custom_future_builder.dart';
 import '../../../../core/widgets/loading.dart';
-import '../../../../core/widgets/simple_app_bar.dart';
-import '../../../data/models/addresses_result_model.dart';
-import '../../../data/repositories/customer_address_repository.dart';
-import '../address_detail/widgets/modal_save_address.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/customer_providers/address_notifier.dart';
+import '../address_save/address_save_page.dart';
 import 'widgets/address_list.dart';
 
-class AddressesPage extends StatefulWidget {
+class AddressesPage extends StatelessWidget {
   static const route = "/customer_addresses";
 
   @override
-  _AddressesPageState createState() => _AddressesPageState();
-}
-
-class _AddressesPageState extends State<AddressesPage> {
-  final _customerAddressRepo = CustomerAddressRepository();
-
-  @override
   Widget build(BuildContext context) {
+    var consumer = Consumer<AddressNotifier>(
+      builder: (context, provider, child) {
+        if (provider.addressesResultModel == null &&
+            provider.listErrorMsg == null) {
+          provider.fetchAddresses();
+        }
+
+        return provider.listLoading
+            ? AppLoading(color: AppTheme.customerPrimary)
+            : provider.listErrorMsg != null
+                ? Txt(
+                    provider.listErrorMsg,
+                    style: AppTxtStyles().body..alignment.center(),
+                  )
+                : provider.addressList.isEmpty
+                    ? Txt(
+                        "لیست آدرس‌های شما خالی است",
+                        style: AppTxtStyles().body..alignment.center(),
+                      )
+                    : AddressList(addresses: provider.addressList);
+      },
+    );
+
     return Scaffold(
-      appBar: SimpleAppBar.intance(
+      appBar: SimpleAppBar().create(
+        context,
         text: "آدرس‌های من",
         showBackBtn: true,
       ),
-      body: futureBuilder,
-      floatingActionButton: fab,
-    );
-  }
-
-  Widget get futureBuilder {
-    return CustomFutureBuilder<Either<Failure, AddressesResultModel>>(
-      future: _customerAddressRepo.addresses,
-      successBuilder: (context, data) {
-        return data.fold(
-          (l) => Txt(
-            l.message,
-            style: AppTxtStyles().body..alignment.center(),
-          ),
-          (r) => AddressList(
-            addresses: r.data,
-            customerAddressRepo: _customerAddressRepo,
-            onActionsComplete: () => setState(() {}),
-          ),
-        );
-      },
-      errorBuilder: (context, data) {
-        return AppLoading(color: AppTheme.customerPrimary);
-      },
-    );
-  }
-
-  // floating action button
-  Widget get fab {
-    return FloatingActionButton(
-      onPressed: fabOnTap,
-      child: Icon(
-        Feather.plus,
-        color: Colors.white,
-        size: 24,
+      body: consumer,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Routes.sailor.navigate(
+            AddressSavePage.route,
+            params: {},
+          );
+        },
+        child: Icon(
+          Feather.plus,
+          color: Colors.white,
+          size: 24,
+        ),
       ),
     );
-  }
-
-  void fabOnTap() {
-    showDialog(
-      context: context,
-      useSafeArea: true,
-      barrierDismissible: true,
-      barrierColor: Colors.black.withOpacity(0.2),
-      builder: (context) {
-        return SaveAddressModal(
-          customerAddressRepo: _customerAddressRepo,
-        );
-      },
-    ).then((value) => setState(() {}));
   }
 }
