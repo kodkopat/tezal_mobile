9dae5c5be2a307a44de553d274b9a0022f67a617
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-03 20:52:58 +0430

    Update address_detail_actions_menu.dart

--- lib/features/presentation/customer_pages/address_detail/widgets/address_detail_actions_menu.dart
+++ lib/features/presentation/customer_pages/address_detail/widgets/address_detail_actions_menu.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:sailor/sailor.dart';
 
+import '../../../../../core/page_routes/routes.dart';
+import '../../../providers/customer_providers/address_notifier.dart';
+import '../../address_save/address_save_page.dart';
 import 'address_detail_actions_menu_item.dart';
+import 'modal_remove_address.dart';
 
 class AddressDetailActionsMenu extends StatelessWidget {
   const AddressDetailActionsMenu({
-    Key key,
-    @required this.onSetDefault,
-    @required this.onEdit,
-    @required this.onRemove,
-  }) : super(key: key);
+    @required this.addressId,
+    @required this.addressNotifier,
+  });
 
-  final void Function() onSetDefault;
-  final void Function() onEdit;
-  final void Function() onRemove;
+  final String addressId;
+  final AddressNotifier addressNotifier;
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +28,51 @@ class AddressDetailActionsMenu extends StatelessWidget {
           AddressDetailActionsMenuItem(
             text: "تنظیم بعنوان پیش‌فرض",
             iconData: Feather.check_square,
-            onTap: onSetDefault,
+            onTap: () async {
+              await addressNotifier.setAddressDefault(addressId: addressId);
+              Routes.sailor.pop();
+            },
           ),
           AddressDetailActionsMenuItem(
             text: "ویرایش اطلاعات آدرس",
             iconData: Feather.edit_3,
-            onTap: onEdit,
+            onTap: () {
+              Routes.sailor.navigate(
+                AddressSavePage.route,
+                navigationType: NavigationType.pushReplace,
+                params: {
+                  "id": null,
+                  "name": null,
+                  "address": null,
+                  "description": null,
+                },
+              );
+            },
           ),
           AddressDetailActionsMenuItem(
             text: "حذف آدرس",
             iconData: Feather.trash_2,
             color: Colors.red,
-            onTap: onRemove,
+            onTap: () {
+              showDialog(
+                context: context,
+                useSafeArea: true,
+                barrierDismissible: true,
+                barrierColor: Colors.black.withOpacity(0.2),
+                builder: (context) {
+                  return RemoveAddressModal(
+                    onAccept: () async {
+                      await addressNotifier.removeAddress(addressId: addressId);
+                      Routes.sailor.pop();
+                      Routes.sailor.pop();
+                    },
+                    onDiscard: () {
+                      Routes.sailor.pop();
+                    },
+                  );
+                },
+              );
+            },
           ),
         ],
       ),
