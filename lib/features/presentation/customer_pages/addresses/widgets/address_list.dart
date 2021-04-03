79b58ee3000432957caaaa33444da4fa2919410a
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-03 20:53:05 +0430

    Update address_list.dart

--- lib/features/presentation/customer_pages/addresses/widgets/address_list.dart
+++ lib/features/presentation/customer_pages/addresses/widgets/address_list.dart
@@ -1,50 +1,31 @@
-import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/page_routes/routes.dart';
-import '../../../../../core/styles/txt_styles.dart';
 import '../../../../data/models/addresses_result_model.dart';
-import '../../../../data/repositories/customer_address_repository.dart';
 import '../../address_detail/address_detail_page.dart';
 import 'adderss_list_item.dart';
 
 class AddressList extends StatelessWidget {
-  const AddressList({
-    Key key,
-    @required this.addresses,
-    @required this.customerAddressRepo,
-    @required this.onActionsComplete,
-  }) : super(key: key);
+  const AddressList({@required this.addresses});
 
   final List<Address> addresses;
-  final CustomerAddressRepository customerAddressRepo;
-  final void Function() onActionsComplete;
 
   @override
   Widget build(BuildContext context) {
-    return addresses.isEmpty
-        ? _emptyState
-        : ListView.builder(
-            itemCount: addresses.length,
-            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            itemBuilder: (context, index) {
-              var address = addresses[index];
-
-              return AddressListItem(
-                onTap: () {
-                  Routes.sailor.navigate(
-                    AddressDetailPage.route,
-                    params: {"addressId": address.id},
-                  );
-                },
-                address: address,
-              );
-            },
-          );
+    return ListView.builder(
+      itemCount: addresses.length,
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      itemBuilder: (context, index) {
+        return AddressListItem(
+          address: addresses[index],
+          onTap: () {
+            Routes.sailor.navigate(
+              AddressDetailPage.route,
+              params: {"addressId": addresses[index].id},
+            );
+          },
+        );
+      },
+    );
   }
-
-  Widget get _emptyState => Txt(
-        "لیست‌ آدرس‌های شما خالی است",
-        style: AppTxtStyles().body..alignment.center(),
-      );
 }
