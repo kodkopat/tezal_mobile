88d70a3eb68ce0654502d5ec0543f124ab975233
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-04 19:01:44 +0430

    Create login_page.dart

---
+++ lib/features/presentation/base_pages/login/login_page.dart
@@ -0,0 +1,196 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:sailor/sailor.dart';
+
+import '../../../../core/page_routes/routes.dart';
+import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/themes/app_theme.dart';
+import '../../../../core/validators/validators.dart';
+import '../../../../core/widgets/action_btn.dart';
+import '../../../../core/widgets/custom_text_input.dart';
+import '../../../data/repositories/auth_repository.dart';
+import '../../base_widgets/agreement_text.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+import '../registeration/registration_page.dart';
+import '../reset_password/reset_password_page.dart';
+
+class LoginPage extends StatefulWidget {
+  static const route = "/login";
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final repository = AuthRepository();
+  final formKey = GlobalKey<FormState>();
+  final phoneCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+
+  String errorTxt = "";
+  bool errorVisibility = false;
+
+  ProgressDialog prgDialog;
+
+  var textStyle = TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.5,
+    fontFamily: 'Yekan',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    prgDialog = ProgressDialog(
+      context,
+      isDismissible: false,
+      type: ProgressDialogType.Normal,
+      textDirection: TextDirection.rtl,
+    )..style(
+        message: "لطفا کمی صبر کنید",
+        textAlign: TextAlign.start,
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: SimpleAppBar().create(
+        context,
+        text: "ورود به حساب کاربری",
+        showBackBtn: true,
+      ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      CustomTextInput(
+                        controller: phoneCtrl,
+                        validator: AppValidators.phone,
+                        label: "شماره موبایل",
+                        maxLength: 11,
+                        textDirection: TextDirection.rtl,
+                        keyboardType: TextInputType.number,
+                      ),
+                      const SizedBox(height: 8),
+                      CustomTextInput(
+                        controller: passCtrl,
+                        validator: AppValidators.pass,
+                        label: "رمز عبور",
+                        maxLength: 4,
+                        textDirection: TextDirection.rtl,
+                        keyboardType: TextInputType.text,
+                      ),
+                      const SizedBox(height: 24),
+                      ActionBtn(
+                        text: "ورود به حساب کاربری",
+                        onTap: onLoginBtnTap,
+                        background: AppTheme.customerPrimary,
+                        textColor: Colors.white,
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        textDirection: TextDirection.rtl,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Txt(
+                            "ایجاد حساب کاربری جدید",
+                            gesture: Gestures()..onTap(onRegistrationBtnTap),
+                            style: AppTxtStyles().footNote.clone()
+                              ..alignmentContent.centerRight()
+                              ..height(24)
+                              ..borderRadius(all: 2)
+                              ..padding(horizontal: 4)
+                              ..ripple(true),
+                          ),
+                          Txt(
+                            "فراموش کردن رمز عبور",
+                            gesture: Gestures()..onTap(onForgetPassBtnTap),
+                            style: AppTxtStyles().footNote.clone()
+                              ..alignmentContent.centerLeft()
+                              ..height(24)
+                              ..borderRadius(all: 2)
+                              ..padding(horizontal: 4)
+                              ..ripple(true),
+                          ),
+                        ],
+                      ),
+                      Visibility(
+                        visible: errorVisibility,
+                        child: Txt(
+                          errorTxt,
+                          style: AppTxtStyles().footNote.clone()
+                            ..textColor(Theme.of(context).errorColor)
+                            ..alignmentContent.center()
+                            ..height(24)
+                            ..borderRadius(all: 2)
+                            ..padding(horizontal: 4)
+                            ..ripple(true),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              AgreementText(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> onLoginBtnTap() async {
+    if (formKey.currentState.validate()) {
+      prgDialog.show();
+      var result = await repository.userLogin(
+        username: phoneCtrl.text,
+        password: passCtrl.text,
+      );
+
+      result.fold(
+        (l) {
+          prgDialog.hide();
+
+          formKey.currentState.reset();
+          setState(() {
+            errorTxt = l.message;
+            errorVisibility = true;
+          });
+        },
+        (r) {
+          prgDialog.hide();
+
+          // App.restart(context);
+          Routes.sailor.pop();
+          Routes.sailor.navigate(
+            "/",
+            navigationType: NavigationType.pushReplace,
+          );
+        },
+      );
+    }
+  }
+
+  void onForgetPassBtnTap() async {
+    Routes.sailor(ResetPasswordPage.route);
+  }
+
+  void onRegistrationBtnTap() {
+    Routes.sailor.navigate(
+      RegistrationPage.route,
+      navigationType: NavigationType.pushReplace,
+    );
+  }
+}
