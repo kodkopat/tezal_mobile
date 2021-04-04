27e9403d4f4dd49ad1e7ec8a359b6e4806a8db53
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-04 19:03:07 +0430

    add some route to Sailor file

--- lib/core/page_routes/routes.dart
+++ lib/core/page_routes/routes.dart
@@ -1,5 +1,11 @@
 import 'package:sailor/sailor.dart';
 
+import '../../features/presentation/base_pages/confirm_registration/confirm_registration.dart';
+import '../../features/presentation/base_pages/confirm_reset_password/confirm_reset_password.dart';
+import '../../features/presentation/base_pages/login/login_page.dart';
+import '../../features/presentation/base_pages/registeration/registration_page.dart';
+import '../../features/presentation/base_pages/reset_password/reset_password_page.dart';
+import '../../features/presentation/base_pages/splash/splash_page.dart';
 import '../../features/presentation/customer_pages/about_us/about_us_page.dart';
 import '../../features/presentation/customer_pages/address_detail/address_detail_page.dart';
 import '../../features/presentation/customer_pages/address_save/address_save_page.dart';
@@ -15,7 +21,6 @@ import '../../features/presentation/customer_pages/product_comments/product_comm
 import '../../features/presentation/customer_pages/product_detail/product_detail_page.dart';
 import '../../features/presentation/customer_pages/profile/profile_page.dart';
 import '../../features/presentation/customer_pages/search/search_page.dart';
-import '../../features/presentation/splash/splash_page.dart';
 
 class Routes {
   static final sailor = Sailor();
@@ -26,6 +31,36 @@ class Routes {
         SailorRoute(
           name: SplashPage.route,
           builder: (ctx, args, map) => SplashPage(),
+        ),
+        SailorRoute(
+          name: LoginPage.route,
+          builder: (ctx, args, map) => LoginPage(),
+        ),
+        SailorRoute(
+          name: RegistrationPage.route,
+          builder: (ctx, args, map) => RegistrationPage(),
+        ),
+        SailorRoute(
+          name: ConfirmRegistrationPage.route,
+          builder: (ctx, args, map) => ConfirmRegistrationPage(),
+        ),
+        SailorRoute(
+          name: ResetPasswordPage.route,
+          builder: (ctx, args, map) => ResetPasswordPage(),
+        ),
+        SailorRoute(
+          name: ConfirmResetPasswordPage.route,
+          builder: (ctx, args, map) {
+            final phone = map.param<String>("phone");
+            return ConfirmResetPasswordPage(phone: phone);
+          },
+          params: [
+            SailorParam<String>(
+              name: "phone",
+              isRequired: true,
+              defaultValue: null,
+            ),
+          ],
         ),
       ],
     );
