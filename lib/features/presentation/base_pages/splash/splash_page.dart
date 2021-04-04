1a8e11c6ca21f3e4686ff47d81e8adfd0f235412
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-04 19:02:06 +0430

    Create splash_page.dart

---
+++ lib/features/presentation/base_pages/splash/splash_page.dart
@@ -0,0 +1,99 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/page_routes/routes.dart';
+import '../../../../core/styles/txt_styles.dart';
+import '../../../data/repositories/auth_repository.dart';
+import '../../customer_pages/dashboard/dashboard_page.dart';
+
+class SplashPage extends StatefulWidget {
+  static const route = "/";
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  final repository = AuthRepository();
+
+  void initializeState() async {
+    final _authRepository = AuthRepository();
+    _AppUserType userType;
+
+    final userId = await repository.userId;
+    print("userId = $userId\n");
+
+    final userToken = await repository.userToken;
+    print("userToken = $userToken\n");
+
+    final value = await _authRepository.userType;
+    if (value != null && value.isNotEmpty) {
+      userType = _AppUserTypeParser.fromString(value);
+    } else {
+      userType = _AppUserType.Customer;
+    }
+
+    switch (userType) {
+      case _AppUserType.Customer:
+        Routes.sailor(DashBoardPage.route);
+        break;
+      case _AppUserType.Market:
+        // Routes.sailor(CustomerDashBoardPage.route);
+        break;
+      case _AppUserType.Delivery:
+        // Routes.sailor(CustomerDashBoardPage.route);
+        break;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Txt(
+          "خوش آمدید",
+          style: AppTxtStyles().body,
+        ),
+      ),
+    );
+  }
+}
+
+enum _AppUserType {
+  Customer,
+  Market,
+  Delivery,
+}
+
+class _AppUserTypeParser {
+  static const _customerKey = "customer";
+  static const _marketKey = "market";
+  static const _deliveryKey = "delivery";
+
+  static _AppUserType fromString(String userType) {
+    if (userType == null || userType.trim().isEmpty)
+      return _AppUserType.Customer;
+
+    switch (userType.toLowerCase()) {
+      case _customerKey:
+        return _AppUserType.Customer;
+        break;
+      case _marketKey:
+        return _AppUserType.Market;
+        break;
+      case _deliveryKey:
+        return _AppUserType.Delivery;
+        break;
+      default:
+        return _AppUserType.Customer;
+        break;
+    }
+  }
+}
