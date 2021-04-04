31f4642393a2aa70d2d07127fdf1ece56e6a57b9
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-04 19:01:39 +0430

    Create confirm_reset_password.dart

---
+++ lib/features/presentation/base_pages/confirm_reset_password/confirm_reset_password.dart
@@ -0,0 +1,193 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:keyboard_avoider/keyboard_avoider.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+
+import '../../../../core/page_routes/routes.dart';
+import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/themes/app_theme.dart';
+import '../../../../core/validators/validators.dart';
+import '../../../../core/widgets/action_btn.dart';
+import '../../../../core/widgets/custom_text_input.dart';
+import '../../../data/repositories/auth_repository.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+
+class ConfirmResetPasswordPage extends StatefulWidget {
+  static const route = "/confirm_reset_password";
+
+  const ConfirmResetPasswordPage({
+    Key key,
+    @required this.phone,
+  }) : super(key: key);
+
+  final String phone;
+
+  @override
+  _ConfirmResetPasswordPageState createState() =>
+      _ConfirmResetPasswordPageState();
+}
+
+class _ConfirmResetPasswordPageState extends State<ConfirmResetPasswordPage> {
+  final repository = AuthRepository();
+  final formKey = GlobalKey<FormState>();
+  final scrollController = ScrollController();
+  var phoneCtrl;
+  final pinCodeCtrl = TextEditingController();
+  final newPassCtrl = TextEditingController();
+  final newPassRepeatCtrl = TextEditingController();
+
+  String errorTxt = "";
+  bool errorVisibility = false;
+
+  ProgressDialog prgDialog;
+
+  var textStyle = TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.5,
+    fontFamily: 'Yekan',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    phoneCtrl = TextEditingController(text: widget.phone);
+
+    prgDialog = ProgressDialog(
+      context,
+      isDismissible: false,
+      type: ProgressDialogType.Normal,
+      textDirection: TextDirection.rtl,
+    )..style(
+        message: "لطفا کمی صبر کنید",
+        textAlign: TextAlign.start,
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: SimpleAppBar().create(
+        context,
+        text: "تغییر رمز عبور",
+        showBackBtn: true,
+      ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: KeyboardAvoider(
+                  child: SingleChildScrollView(
+                    controller: scrollController,
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        AbsorbPointer(
+                          absorbing: true,
+                          child: CustomTextInput(
+                            controller: phoneCtrl,
+                            validator: AppValidators.phone,
+                            label: "شماره موبایل",
+                            maxLength: 11,
+                            textDirection: TextDirection.rtl,
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        CustomTextInput(
+                          controller: pinCodeCtrl,
+                          validator: AppValidators.pass,
+                          label: "کد تایید یکبار مصرف",
+                          maxLength: 4,
+                          textDirection: TextDirection.rtl,
+                          keyboardType: TextInputType.number,
+                        ),
+                        const SizedBox(height: 8),
+                        CustomTextInput(
+                          controller: newPassCtrl,
+                          validator: AppValidators.pass,
+                          label: "رمز عبور جدید",
+                          maxLength: 4,
+                          textDirection: TextDirection.rtl,
+                          keyboardType: TextInputType.number,
+                          obscureText: true,
+                        ),
+                        CustomTextInput(
+                          controller: newPassRepeatCtrl,
+                          validator: AppValidators.pass,
+                          label: "تکرار رمز جدید",
+                          maxLength: 4,
+                          textDirection: TextDirection.rtl,
+                          keyboardType: TextInputType.number,
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 24),
+                        ActionBtn(
+                          text: "ثبت",
+                          onTap: onSubmitBtnTap,
+                          background: AppTheme.customerPrimary,
+                          textColor: Colors.white,
+                        ),
+                        const SizedBox(height: 16),
+                        Visibility(
+                          visible: errorVisibility,
+                          child: Txt(
+                            errorTxt,
+                            style: AppTxtStyles().footNote.clone()
+                              ..textColor(Theme.of(context).errorColor)
+                              ..alignmentContent.center()
+                              ..height(24)
+                              ..borderRadius(all: 2)
+                              ..padding(horizontal: 4)
+                              ..ripple(true),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onSubmitBtnTap() async {
+    if (formKey.currentState.validate()) {
+      prgDialog.show();
+      var result = await repository.resetPass(
+        phone: phoneCtrl.text,
+        sms: pinCodeCtrl.text,
+        password: newPassCtrl.text,
+        passwordRepeat: newPassRepeatCtrl.text,
+      );
+
+      result.fold(
+        (l) {
+          prgDialog.hide();
+
+          formKey.currentState.reset();
+          setState(() {
+            errorTxt = l.message;
+            errorVisibility = true;
+          });
+        },
+        (r) {
+          prgDialog.hide();
+          Routes.sailor.pop();
+          Routes.sailor.pop();
+        },
+      );
+    }
+  }
+}
