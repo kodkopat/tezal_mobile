d6265a8424c15287de2f233cc5686637b072e948
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-05 13:28:12 +0430

    edit agreement modals

--- lib/features/presentation/base_widgets/modal_rules.dart
+++ lib/features/presentation/base_widgets/modal_rules.dart
@@ -1,12 +1,14 @@
 import 'package:dartz/dartz.dart';
+import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 
 import '../../../core/exceptions/failure.dart';
+import '../../../core/styles/txt_styles.dart';
 import '../../../core/themes/app_theme.dart';
 import '../../../core/widgets/custom_future_builder.dart';
 import '../../../core/widgets/loading.dart';
-import '../../data/models/base_api_result_model.dart';
+import '../../data/models/agreement_result_model.dart';
 import '../../data/repositories/auth_repository.dart';
 import '../customer_widgets/simple_app_bar.dart';
 
@@ -23,19 +25,18 @@ class RulesModal extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        child: CustomFutureBuilder<Either<Failure, BaseApiResultModel>>(
+        child: CustomFutureBuilder<Either<Failure, AgreementResultModel>>(
           future: repository.privacyText,
           successBuilder: (context, data) {
             return data.fold(
               (left) {
-                // return Txt(
-                //   "${left.message}",
-                //   style: AppTxtStyles().body..textAlign.justify(),
-                // );
-                return Html(data: "${left.message}");
+                return Txt(
+                  "${left.message}",
+                  style: AppTxtStyles().body..textAlign.justify(),
+                );
               },
               (right) {
-                return Html(data: right.data);
+                return Html(data: right.data.message);
               },
             );
           },
