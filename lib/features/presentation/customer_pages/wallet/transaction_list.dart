b9d78a369102a52df0915ba93ec87cca4098f449
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-05 13:28:44 +0430

    create Wallet Transactions List

---
+++ lib/features/presentation/customer_pages/wallet/transaction_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tezal/features/data/models/wallet_detail_result_model.dart'
+    as wallet;
+
+import 'transaction_list_item.dart';
+
+class TransactionList extends StatelessWidget {
+  const TransactionList({@required this.walletDetail});
+
+  final List<wallet.Detail> walletDetail;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: walletDetail.length,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return TransactionListItem(walletDetail: walletDetail[index]);
+      },
+    );
+  }
+}
---
+++ lib/features/presentation/customer_pages/wallet/transaction_list_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tezal/features/data/models/wallet_detail_result_model.dart'
+    as wallet;
+
+class TransactionListItem extends StatelessWidget {
+  const TransactionListItem({@required this.walletDetail});
+
+  final wallet.Detail walletDetail;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
