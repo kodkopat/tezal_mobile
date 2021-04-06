cf8d6ec70716236e13d7f54a2fc9b4a29ed6b887
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-06 20:08:35 +0430

    Update customer_order_repository.dart

--- lib/features/data/repositories/customer_order_repository.dart
+++ lib/features/data/repositories/customer_order_repository.dart
@@ -1,8 +1,16 @@
+import 'package:dartz/dartz.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
 
+import '../../../core/exceptions/api_failure.dart';
+import '../../../core/exceptions/connection_failure.dart';
+import '../../../core/exceptions/failure.dart';
 import '../data_sources/customer_order/customer_order_local_data_source.dart';
 import '../data_sources/customer_order/customer_order_remote_data_source.dart';
+import '../models/older_orders_result_model.dart';
+import '../models/order_detail_result_model.dart';
+import '../models/order_result_model.dart';
 import 'auth_repository.dart';
 
 class CustomerOrderRepository {
@@ -18,4 +26,41 @@ class CustomerOrderRepository {
   // ignore: unused_field
   final CustomerOrderLocalDataSource _localDataSource;
   final AuthRepository _authRepo;
+
+  Future<Either<Failure, OrderResultModel>> saveOrder({
+    @required int paymentType,
+  }) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      final userToken = await _authRepo.userToken;
+      var result = await _remoteDataSource.save(userToken, paymentType);
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+
+  Future<Either<Failure, OlderOrdersResultModel>> olderOrders() async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      final userToken = await _authRepo.userToken;
+      var result = await _remoteDataSource.getOlderOrders(userToken);
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+
+  Future<Either<Failure, OrderDetailResultModel>> orderDetail({
+    @required String id,
+  }) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      final userToken = await _authRepo.userToken;
+      var result = await _remoteDataSource.getDetail(userToken, id);
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
 }
