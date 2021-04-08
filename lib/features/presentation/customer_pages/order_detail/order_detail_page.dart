e0d04cb5fae889e35dae05fcdb717456d1d9a3d6
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-08 22:07:08 +0430

    Create order_detail_page.dart

---
+++ lib/features/presentation/customer_pages/order_detail/order_detail_page.dart
@@ -0,0 +1,50 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/themes/app_theme.dart';
+import '../../../../core/widgets/loading.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/customer_providers/order_notifier.dart';
+import 'widgets/order_list.dart';
+
+class OrderDetailPage extends StatelessWidget {
+  static const route = "/customer_order_detail";
+
+  const OrderDetailPage({@required this.orderId});
+
+  final String orderId;
+
+  @override
+  Widget build(BuildContext context) {
+    var consumer = Consumer<OrderNotifier>(
+      builder: (context, provider, child) {
+        if (provider.orderDetailResultModel == null &&
+            provider.orderDetailErrorMsg == null) {
+          provider.fetchOrderDetail(orderId: orderId);
+        }
+
+        return provider.orderDetailLoading
+            ? AppLoading(color: AppTheme.customerPrimary)
+            : provider.orderDetailErrorMsg != null
+                ? Txt(
+                    provider.orderDetailErrorMsg,
+                    style: AppTxtStyles().body..alignment.center(),
+                  )
+                : OrderList(
+                    orderItems: provider.orderDetailResultModel.data?.items,
+                    orderNotifier: provider,
+                  );
+      },
+    );
+
+    return Scaffold(
+      appBar: SimpleAppBar(context).create(
+        text: "جزئیات سفارش",
+        showBackBtn: true,
+      ),
+      body: consumer,
+    );
+  }
+}
