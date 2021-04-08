d410ba38a037d729e1996d86aa9b3f1e518c0c05
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-08 22:05:08 +0430

    Update product_notifier.dart

--- lib/features/presentation/providers/customer_providers/product_notifier.dart
+++ lib/features/presentation/providers/customer_providers/product_notifier.dart
@@ -1,9 +1,64 @@
 import 'package:flutter/material.dart';
 
+import '../../../data/models/liked_products_result_model.dart';
 import '../../../data/repositories/customer_product_repository.dart';
 
 class ProductNotifier extends ChangeNotifier {
-  ProductNotifier(this.customerProductRepo);
+  static ProductNotifier _instance;
+
+  factory ProductNotifier(
+    CustomerProductRepository customerProductRepo,
+  ) {
+    if (_instance == null) {
+      _instance = ProductNotifier._privateConstructor(
+        customerProductRepo: customerProductRepo,
+      );
+    }
+
+    return _instance;
+  }
+
+  ProductNotifier._privateConstructor({
+    this.customerProductRepo,
+  });
 
   final CustomerProductRepository customerProductRepo;
+
+  bool likedProductsloading = true;
+  String likedProductsErrorMsg;
+  LikedProductsResultModel likedProductsResultModel;
+
+  Future<void> fetchLikedProducts() async {
+    var result = await customerProductRepo.likedProdutcs();
+
+    result.fold(
+      (left) => likedProductsErrorMsg = left.message,
+      (right) => likedProductsResultModel = right,
+    );
+
+    likedProductsloading = false;
+    notifyListeners();
+  }
+
+  Future<void> likeProduct({@required String productId}) async {
+    var result = await customerProductRepo.likeProduct(id: productId);
+
+    result.fold(
+      (left) => null,
+      (right) => refresh(),
+    );
+  }
+
+  Future<void> unlikeProduct({@required String productId}) async {
+    var result = await customerProductRepo.unlikeProduct(id: productId);
+
+    result.fold(
+      (left) => null,
+      (right) => refresh(),
+    );
+  }
+
+  void refresh() async {
+    await fetchLikedProducts();
+  }
 }
