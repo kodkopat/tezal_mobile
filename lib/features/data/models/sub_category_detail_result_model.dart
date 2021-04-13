4e7678afbb9cbb9d4c95d03f91d9b2af85d73bac
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-13 18:25:32 +0430

    Create sub_category_detail_result_model.dart

---
+++ lib/features/data/models/sub_category_detail_result_model.dart
@@ -0,0 +1,63 @@
+// To parse this JSON data, do
+//
+//     final subCategoryDetailResultModel = subCategoryDetailResultModelFromJson(jsonString);
+
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+
+import 'product_result_model.dart';
+
+class SubCategoryDetailResultModel {
+  SubCategoryDetailResultModel({
+    @required this.success,
+    @required this.message,
+    @required this.data,
+  });
+
+  final success;
+  final message;
+  final data;
+
+  factory SubCategoryDetailResultModel.fromJson(Map<String, dynamic> json) =>
+      SubCategoryDetailResultModel(
+        success: json["success"],
+        message: json["message"],
+        data: Data.fromJson(json["data"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
+        "data": data.toJson(),
+      };
+}
+
+class Data {
+  Data({
+    @required this.categoryName,
+    @required this.categoryId,
+    @required this.products,
+  });
+
+  final categoryName;
+  final categoryId;
+  final products;
+
+  factory Data.fromRawJson(String str) => Data.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        categoryName: json["categoryName"],
+        categoryId: json["categoryId"],
+        products: List<ProductResultModel>.from(
+            json["products"].map((x) => ProductResultModel.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "categoryName": categoryName,
+        "categoryId": categoryId,
+        "products": List<dynamic>.from(products.map((x) => x.toJson())),
+      };
+}
