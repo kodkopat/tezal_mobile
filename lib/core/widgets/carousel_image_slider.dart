840a113d0da9e6cc025a8f1bbcdd83a1c1790580
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-13 18:23:56 +0430

    create carousel image slider

---
+++ lib/core/widgets/carousel_image_slider.dart
@@ -0,0 +1,112 @@
+import 'dart:convert';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+
+import '../styles/txt_styles.dart';
+import 'carousel_image_slider_modal.dart';
+
+class CarouselImageSlider extends StatefulWidget {
+  const CarouselImageSlider({required this.images});
+
+  final List<String> images;
+
+  @override
+  _CarouselImageSliderState createState() => _CarouselImageSliderState();
+}
+
+class _CarouselImageSliderState extends State<CarouselImageSlider> {
+  int selectedImage = 0;
+  final double boxHeight = 160;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Parent(
+          style: ParentStyle()
+            ..background.color(Colors.white)
+            ..borderRadius(all: 8)
+            ..boxShadow(
+              color: Colors.black.withOpacity(0.2),
+              offset: Offset(0, 4.0),
+              blur: 8,
+              spread: 0,
+            ),
+          child: SizedBox(
+            height: boxHeight,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+              child: PageView.builder(
+                pageSnapping: true,
+                itemCount: widget.images.length,
+                onPageChanged: (value) {
+                  setState(() {
+                    selectedImage = value;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return Parent(
+                    gesture: Gestures()
+                      ..onTap(() {
+                        showDialog(
+                          context: context,
+                          barrierColor: Colors.black,
+                          builder: (context) {
+                            return ModalCarouselImageSlider(
+                              imagePathList: widget.images,
+                              selectedIndex: selectedImage,
+                            );
+                          },
+                        );
+                      }),
+                    style: ParentStyle()
+                      ..width(MediaQuery.of(context).size.width)
+                      ..height(boxHeight)
+                      ..background.image(
+                        alignment: Alignment.center,
+                        path: "assets/images/placeholder.jpg",
+                        fit: BoxFit.fill,
+                      ),
+                    child: Image.memory(
+                      base64Decode(widget.images[index]),
+                      fit: BoxFit.fill,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 8,
+          child: Container(
+            height: 16,
+            alignment: Alignment.center,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: widget.images.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Txt(
+                  "\u2B24",
+                  style: AppTxtStyles().footNote
+                    ..clone()
+                    ..padding(horizontal: 2)
+                    ..textColor(
+                      selectedImage == index ? Colors.yellow : Colors.grey,
+                    ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/core/widgets/carousel_image_slider_modal.dart
@@ -0,0 +1,121 @@
+import 'dart:convert';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:tezal/core/page_routes/routes.dart';
+
+import '../styles/txt_styles.dart';
+
+class ModalCarouselImageSlider extends StatefulWidget {
+  const ModalCarouselImageSlider({
+    required this.imagePathList,
+    required this.selectedIndex,
+  });
+
+  final List<String> imagePathList;
+  final int selectedIndex;
+
+  @override
+  _ModalCarouselImageSliderState createState() =>
+      _ModalCarouselImageSliderState();
+}
+
+class _ModalCarouselImageSliderState extends State<ModalCarouselImageSlider> {
+  late int selectedIndex;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedIndex = widget.selectedIndex;
+    pageController = PageController(
+      keepPage: true,
+      initialPage: selectedIndex,
+      viewportFraction: 1.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return Stack(
+      children: [
+        SizedBox(
+          width: screenSize.width,
+          height: screenSize.height,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: widget.imagePathList.length,
+            onPageChanged: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Image.memory(
+                base64Decode(widget.imagePathList[index]),
+                fit: BoxFit.contain,
+              );
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 32,
+          left: 0,
+          right: 0,
+          child: _buildCarouselSliderIndicators(),
+        ),
+        Positioned(
+          top: 16,
+          left: 16,
+          child: _buildCloseBtn(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCarouselSliderIndicators() {
+    return Container(
+      height: 16,
+      alignment: Alignment.center,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: widget.imagePathList.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Txt(
+            "\u2B24",
+            style: AppTxtStyles().footNote
+              ..clone()
+              ..padding(horizontal: 2)
+              ..textColor(
+                selectedIndex == index
+                    ? Colors.white
+                    : Colors.grey.withOpacity(0.75),
+              ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildCloseBtn() {
+    return Parent(
+      gesture: Gestures()..onTap(() => Routes.sailor.pop()),
+      style: ParentStyle()
+        ..width(48)
+        ..height(48)
+        ..borderRadius(all: 24)
+        ..ripple(true),
+      child: Icon(
+        Feather.x,
+        color: Colors.white,
+        size: 24,
+      ),
+    );
+  }
+}
