3f233bc7afb2a9f87568d823d3e714fe9e716775
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-13 18:28:38 +0430

    Create campaign_notifier.dart

---
+++ lib/features/presentation/providers/customer_providers/campaign_notifier.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/campaign_result_model.dart';
+import '../../../data/repositories/customer_campaign_repository.dart';
+
+class CampaignNotifier extends ChangeNotifier {
+  static CampaignNotifier? _instance;
+
+  factory CampaignNotifier(
+    CustomerCampaignRepository customerCampaignRepo,
+  ) {
+    if (_instance == null) {
+      _instance = CampaignNotifier._privateConstructor(
+        customerCampaignRepo: customerCampaignRepo,
+      );
+    }
+
+    return _instance!;
+  }
+
+  CampaignNotifier._privateConstructor({
+    required this.customerCampaignRepo,
+  });
+
+  final CustomerCampaignRepository customerCampaignRepo;
+
+  bool campaignLoading = true;
+  String? campaignErrorMsg;
+  List<Campaign>? campaigns;
+
+  Future<void> fetchCampaigns() async {
+    var result = await customerCampaignRepo.campaignes;
+
+    result.fold(
+      (left) => campaignErrorMsg = left.message,
+      (right) => campaigns = right.data,
+    );
+
+    campaignLoading = false;
+    notifyListeners();
+  }
+}
