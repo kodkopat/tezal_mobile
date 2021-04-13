b7641ec40efc7bdedb98c10c7b1fb0d601798966
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-13 18:25:06 +0430

    Create customer_category_repository.dart

---
+++ lib/features/data/repositories/customer_category_repository.dart
@@ -0,0 +1,52 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:dartz/dartz.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:dio/dio.dart';
+
+import '../../../core/exceptions/api_failure.dart';
+import '../../../core/exceptions/connection_failure.dart';
+import '../../../core/exceptions/failure.dart';
+import '../data_sources/customer_category/customer_category_local_data_source.dart';
+import '../data_sources/customer_category/customer_category_remote_data_source.dart';
+import '../models/photo_result_model.dart';
+import 'auth_repository.dart';
+
+class CustomerCategoryRepository {
+  CustomerCategoryRepository()
+      : _connectionChecker = DataConnectionChecker(),
+        _remoteDataSource = CustomerCategoryRemoteDataSource(Dio()),
+        _localDataSource = CustomerCategoryLocalDataSource(),
+        _authRepo = AuthRepository();
+
+  final String connectionFailedMsg = "دسترسی به اینترنت امکان‌پذیر نمی‌باشد!";
+  final DataConnectionChecker _connectionChecker;
+  final CustomerCategoryRemoteDataSource _remoteDataSource;
+  // ignore: unused_field
+  final CustomerCategoryLocalDataSource _localDataSource;
+  final AuthRepository _authRepo;
+
+  Future<Either<Failure, PhotoResultModel>> mainCategoryPhoto(
+      {required String id}) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      final userToken = await _authRepo.userToken;
+      var result = await _remoteDataSource.getMainCategoryPhoto(userToken, id);
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+
+  Future<Either<Failure, PhotoResultModel>> subCategoryPhoto(
+      {required String id}) async {
+    if (!await _connectionChecker.hasConnection) {
+      return Left(ConnectionFailure(connectionFailedMsg));
+    } else {
+      final userToken = await _authRepo.userToken;
+      var result = await _remoteDataSource.getSubCategoryPhoto(userToken, id);
+
+      return result.success ? Right(result) : Left(ApiFailure(result.message));
+    }
+  }
+}
