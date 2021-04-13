48e878033f935102acf3bc02b69d9d0645987d58
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-13 18:24:14 +0430

    Update progress_dialog.dart

--- lib/core/widgets/progress_dialog.dart
+++ lib/core/widgets/progress_dialog.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:progress_dialog/progress_dialog.dart';
 
-import 'loading.dart';
-
 class AppProgressDialog {
-  final BuildContext context;
-
   AppProgressDialog(this.context);
 
+  final BuildContext context;
+
   ProgressDialog get instance {
-    return ProgressDialog(context,
-        isDismissible: true,
-        type: ProgressDialogType.Normal,
-        customBody: AppLoading())
-      ..style(
-        backgroundColor: Colors.black,
-        progressWidgetAlignment: Alignment.center,
+    return ProgressDialog(
+      context,
+      isDismissible: true,
+      type: ProgressDialogType.Normal,
+      textDirection: TextDirection.rtl,
+    )..style(
+        message: "لطفا کمی صبر کنید",
+        textAlign: TextAlign.start,
       );
   }
 }
