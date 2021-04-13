52947a2908427579ea0142850879df32b002638b
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-13 18:27:54 +0430

    Update wallet_page.dart

--- lib/features/presentation/customer_pages/wallet/wallet_page.dart
+++ lib/features/presentation/customer_pages/wallet/wallet_page.dart
@@ -3,12 +3,12 @@ import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
 import 'package:provider/provider.dart';
-import 'package:tezal/core/widgets/load_more_btn.dart';
 
 import '../../../../core/page_routes/routes.dart';
 import '../../../../core/styles/txt_styles.dart';
 import '../../../../core/themes/app_theme.dart';
 import '../../../../core/widgets/action_btn.dart';
+import '../../../../core/widgets/load_more_btn.dart';
 import '../../../../core/widgets/loading.dart';
 import '../../customer_widgets/simple_app_bar.dart';
 import '../../providers/customer_providers/wallet_notifier.dart';
@@ -22,22 +22,24 @@ class WalletPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var walltInfoConsumer = Consumer<WalletNotifier>(
       builder: (context, provider, child) {
-        if (provider.walletInfoResultModel == null &&
-            provider.infoErrorMsg == null) {
+        if (provider.walletInfoResultModel == null) {
           provider.fetchWalletInfo();
         }
 
         return provider.infoLoading
             ? AppLoading(color: AppTheme.customerPrimary)
-            : provider.infoErrorMsg != null
-                ? Txt(
-                    provider.infoErrorMsg!,
-                    style: AppTxtStyles().body..alignment.center(),
-                  )
+            : provider.walletInfoResultModel == null
+                ? provider.infoErrorMsg == null
+                    ? Txt(
+                        "خطای بارگذاری اطلاعات",
+                        style: AppTxtStyles().body..alignment.center(),
+                      )
+                    : Txt(
+                        provider.infoErrorMsg!,
+                        style: AppTxtStyles().body..alignment.center(),
+                      )
                 : Parent(
-                    style: ParentStyle()
-                      ..padding(horizontal: 48, vertical: 24)
-                      ..background.color(Color(0xffEFEFEF)),
+                    style: ParentStyle()..padding(all: 24),
                     child: Column(
                       textDirection: TextDirection.rtl,
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -69,42 +71,29 @@ class WalletPage extends StatelessWidget {
 
     var walltDetailConsumer = Consumer<WalletNotifier>(
       builder: (context, provider, child) {
-        if (provider.walletDetailList == null &&
-            provider.detailErrorMsg == null) {
+        if (provider.walletDetailList == null) {
           provider.walletDetail();
         }
 
         return provider.detailLoading
             ? AppLoading(color: AppTheme.customerPrimary)
-            : provider.detailErrorMsg != null
-                ? Txt(
-                    provider.detailErrorMsg!,
-                    style: AppTxtStyles().body..alignment.center(),
-                  )
-                : provider.walletDetailList!.isEmpty
-                    ? Txt(
-                        "لیست تراکنش‌های شما خالی است",
-                        style: AppTxtStyles().body..alignment.center(),
-                      )
-                    : SingleChildScrollView(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            TransactionList(
-                              walletDetail: provider.walletDetailList!,
-                            ),
-                            SizedBox(height: 8),
-                            if (provider.enableLoadMoreData!)
-                              LoadMoreBtn(
-                                onTap: () {
-                                  provider.walletDetail();
-                                },
-                              )
-                          ],
-                        ),
-                      );
+            : provider.walletDetailList == null
+                ? provider.detailErrorMsg == null
+                    ? Txt("لیست تراکنش‌های شما خالی است",
+                        style: AppTxtStyles().body..alignment.center())
+                    : Txt(provider.detailErrorMsg,
+                        style: AppTxtStyles().body..alignment.center())
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TransactionList(walletDetail: provider.walletDetailList!),
+                      const SizedBox(height: 8),
+                      if (provider.enableLoadMoreData!)
+                        LoadMoreBtn(onTap: () {
+                          provider.walletDetail();
+                        })
+                    ],
+                  );
       },
     );
 
@@ -113,14 +102,15 @@ class WalletPage extends StatelessWidget {
         text: "کیف پول من",
         showBackBtn: true,
       ),
-      body: Column(
-        textDirection: TextDirection.rtl,
-        children: [
-          walltInfoConsumer,
-          Expanded(
-            child: walltDetailConsumer,
-          ),
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          textDirection: TextDirection.rtl,
+          children: [
+            walltInfoConsumer,
+            walltDetailConsumer,
+          ],
+        ),
       ),
     );
   }
