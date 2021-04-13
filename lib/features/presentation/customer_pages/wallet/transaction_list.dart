027a89ef420eda45963090d672543040ff9aaed5
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-13 18:27:44 +0430

    Update transaction_list.dart

--- lib/features/presentation/customer_pages/wallet/transaction_list.dart
+++ lib/features/presentation/customer_pages/wallet/transaction_list.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:tezal/features/data/models/wallet_detail_result_model.dart'
-    as wallet;
 
+import '../../../../core/widgets/dashed_line_painter.dart';
+import '../../../../features/data/models/wallet_detail_result_model.dart'
+    as wallet;
 import 'transaction_list_item.dart';
 
 class TransactionList extends StatelessWidget {
@@ -11,7 +12,7 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
       shrinkWrap: true,
       itemCount: walletDetail.length,
       physics: NeverScrollableScrollPhysics(),
@@ -19,6 +20,9 @@ class TransactionList extends StatelessWidget {
         return TransactionListItem(
           walletDetail: walletDetail[index],
         );
+      },
+      separatorBuilder: (context, index) {
+        return CustomPaint(painter: DashedLinePainter());
       },
     );
   }
