0856247b1e5bedc3f366cc6f0be0465cdd4b8c6c
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-14 22:02:01 +0430

    Update liked_product_list_item.dart

--- lib/features/presentation/customer_pages/liked_products/widgets/liked_product_list_item.dart
+++ lib/features/presentation/customer_pages/liked_products/widgets/liked_product_list_item.dart
@@ -10,7 +10,7 @@ import '../../../../../core/styles/txt_styles.dart';
 import '../../../../../core/widgets/custom_future_builder.dart';
 import '../../../../data/models/liked_products_result_model.dart';
 import '../../../../data/models/photos_result_model.dart';
-import '../../../customer_widgets/product_list/product_list_item_like_toggle.dart';
+import '../../../customer_widgets/product_list/product_like_toggle.dart';
 import '../../../providers/customer_providers/product_notifier.dart';
 
 class LikedProductListItem extends StatelessWidget {
