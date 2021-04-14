fd38a0b1dbe77fdf32a6215446a4c2ab55e2d0d2
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-14 22:00:43 +0430

    Create products_notifier.dart

---
+++ lib/features/presentation/providers/customer_providers/products_notifier.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/product_result_model.dart';
+import '../../../data/repositories/customer_product_repository.dart';
+
+class ProductsNotifier extends ChangeNotifier {
+  ProductsNotifier(this.customerProductRepo);
+
+  final CustomerProductRepository customerProductRepo;
+
+  bool productsLoading = true;
+  String? productsErrorMsg;
+  List<ProductResultModel>? products;
+
+  Future<void> fetchProducts({
+    required String marketId,
+    required String categoryId,
+  }) async {
+    var result = await customerProductRepo.productList(
+      marketId: marketId,
+      categoryId: categoryId,
+    );
+
+    result.fold(
+      (left) => productsErrorMsg = left.message,
+      (right) {
+        products = right.data;
+      },
+    );
+
+    notifyListeners();
+  }
+}
