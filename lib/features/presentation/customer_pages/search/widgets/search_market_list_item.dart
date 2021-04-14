9d89f16a75e7919c4f16e156855a953de0079977
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-14 22:01:13 +0430

    Update search_market_list_item.dart

--- lib/features/presentation/customer_pages/search/widgets/search_market_list_item.dart
+++ lib/features/presentation/customer_pages/search/widgets/search_market_list_item.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import '../../../../../core/page_routes/routes.dart';
 import '../../../../../core/styles/txt_styles.dart';
 import '../../../../data/models/search_result_model.dart';
-import '../../../customer_widgets/product_list/product_list.dart';
+import '../../../customer_widgets/product_list/product_horizontal_list.dart';
 import '../../market_detail/market_detail_page.dart';
 
 class SearchMarketListItem extends StatelessWidget {
@@ -47,7 +47,7 @@ class SearchMarketListItem extends StatelessWidget {
             ],
           ),
         ),
-        ProductList(
+        ProductHorizontalList(
           products: market.products,
         ),
       ],
