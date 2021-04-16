0f6c1a0471819ea0caaae2f7194846c4e929dfe6
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-16 20:37:41 +0430

    Create category_notifier.dart

---
+++ lib/features/presentation/providers/customer_providers/category_notifier.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/repositories/customer_category_repository.dart';
+
+class CategoryNotifier extends ChangeNotifier {
+  static CategoryNotifier? _instance;
+
+  factory CategoryNotifier(
+    CustomerCategoryRepository customerCategoryRepo,
+  ) {
+    if (_instance == null) {
+      _instance = CategoryNotifier._privateConstructor(
+        customerCategoryRepo: customerCategoryRepo,
+      );
+    }
+
+    return _instance!;
+  }
+
+  CategoryNotifier._privateConstructor({
+    required this.customerCategoryRepo,
+  });
+
+  final CustomerCategoryRepository customerCategoryRepo;
+}
