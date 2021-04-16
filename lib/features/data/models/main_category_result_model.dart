c7a1760c9c5fa63ee680c844be6d8d01456e98bd
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-16 20:38:46 +0430

    Create main_category_result_model.dart

---
+++ lib/features/data/models/main_category_result_model.dart
@@ -0,0 +1,55 @@
+// To parse this JSON data, do
+//
+//     final mainCategoryResultModel = mainCategoryResultModelFromJson(jsonString);
+
+import 'package:meta/meta.dart';
+
+class MainCategoryResultModel {
+  MainCategoryResultModel({
+    @required this.success,
+    @required this.message,
+    @required this.data,
+  });
+
+  final success;
+  final message;
+  final List<MainCategory>? data;
+
+  factory MainCategoryResultModel.fromJson(Map<String, dynamic> json) =>
+      MainCategoryResultModel(
+        success: json["success"],
+        message: json["message"],
+        data: json["data"] == null
+            ? null
+            : List<MainCategory>.from(
+                json["data"].map((x) => MainCategory.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
+        "data": data == null
+            ? null
+            : List<dynamic>.from(data!.map((x) => x.toJson())),
+      };
+}
+
+class MainCategory {
+  MainCategory({
+    @required this.id,
+    @required this.name,
+  });
+
+  final id;
+  final name;
+
+  factory MainCategory.fromJson(Map<String, dynamic> json) => MainCategory(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+      };
+}
