359b29b1a9fb43f43abe1e4ea29e41d764233ab3
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-18 22:29:21 +0430

    Update market_detail_page.dart

--- lib/features/presentation/customer_pages/market_detail/market_detail_page.dart
+++ lib/features/presentation/customer_pages/market_detail/market_detail_page.dart
@@ -3,24 +3,26 @@ import 'package:dartz/dartz.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../core/exceptions/failure.dart';
 import '../../../../core/page_routes/routes.dart';
 import '../../../../core/styles/txt_styles.dart';
 import '../../../../core/themes/app_theme.dart';
-import '../../../../core/widgets/carousel_image_slider.dart';
 import '../../../../core/widgets/custom_future_builder.dart';
 import '../../../../core/widgets/loading.dart';
 import '../../../data/models/comments_result_model.dart';
 import '../../../data/models/market_detail_result_model.dart';
 import '../../../data/models/nearby_markets_result_model.dart';
 import '../../../data/models/photos_result_model.dart';
-import '../../../data/repositories/customer_market_repository.dart';
 import '../../customer_widgets/category_list/category_list.dart';
 import '../../customer_widgets/comment_list/comment_list.dart';
 import '../../customer_widgets/market_list/markets_list_item.dart';
 import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/customer_providers/market_comments_notifier.dart';
+import '../../providers/customer_providers/market_detail_provider.dart';
 import '../market_comments/market_comments_page.dart';
+import 'widgets/market_slider.dart';
 
 class MarketDetailPage extends StatelessWidget {
   static const route = "/customer_market_detail";
@@ -28,42 +30,45 @@ class MarketDetailPage extends StatelessWidget {
   MarketDetailPage({required this.marketId});
 
   final String marketId;
-
-  final _customerMarketRepo = CustomerMarketRepository();
+  late final MarketCommentsNotifier marketCommentsNotifier;
 
   @override
   Widget build(BuildContext context) {
+    marketCommentsNotifier =
+        Provider.of<MarketCommentsNotifier>(context, listen: false);
+
+    var consumer = Consumer<MarketDetailNotifier>(
+      builder: (context, provider, child) {
+        if (provider.marketDetail == null) {
+          provider.fetchMarketDetail(marketId: marketId);
+        }
+
+        return provider.marketDetailLoading
+            ? AppLoading(color: AppTheme.customerPrimary)
+            : provider.marketDetail == null
+                ? provider.marketDetailErrorMsg == null
+                    ? Txt("خطای بارگذاری اطلاعات",
+                        style: AppTxtStyles().body..alignment.center())
+                    : Txt(provider.marketDetailErrorMsg,
+                        style: AppTxtStyles().body..alignment.center())
+                : _listOfSections(context, provider.marketDetail!);
+      },
+    );
+
     return Scaffold(
       appBar: SimpleAppBar(context).create(
         text: "جزئیات فروشگاه",
         showBackBtn: true,
         showBasketBtn: true,
       ),
-      body: CustomFutureBuilder(
-        future: _customerMarketRepo.marketDetail(
-          marketId: marketId,
-        ),
-        successBuilder: (context, data) {
-          var result = data as Either<Failure, MarketDetailResultModel>;
-
-          return result.fold(
-            (left) => Txt(
-              left.message,
-              style: AppTxtStyles().body..alignment.center(),
-            ),
-            (right) => _listOfSections(right),
-          );
-        },
-        errorBuilder: (context, error) {
-          return AppLoading(
-            color: AppTheme.customerPrimary,
-          );
-        },
-      ),
+      body: consumer,
     );
   }
 
-  Widget _listOfSections(MarketDetailResultModel marketDetail) {
+  Widget _listOfSections(
+    BuildContext context,
+    MarketDetailResultModel marketDetail,
+  ) {
     var categories = marketDetail.data!.categories;
 
     var data = marketDetail.data;
@@ -90,7 +95,8 @@ class MarketDetailPage extends StatelessWidget {
           const SizedBox(height: 16),
           _sectionDetailsBox(market),
           _sectionCategories(categories!),
-          _sectionComments(),
+          _sectionComments(context),
+          const SizedBox(height: 16),
         ],
       ),
     );
@@ -100,15 +106,24 @@ class MarketDetailPage extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: CustomFutureBuilder<Either<Failure, PhotosResultModel>>(
-        future: _customerMarketRepo.photo(marketId: marketId),
+        future:
+            marketCommentsNotifier.customerMarketRepo.photo(marketId: marketId),
         successBuilder: (context, data) {
           return data!.fold(
-            (l) => CarouselImageSlider(images: []),
-            (r) => CarouselImageSlider(images: r.data.photos),
+            (left) => MarketSlider(images: []),
+            (right) {
+              var latitude = "${marketDetail.data!.location}".split("-")[0];
+              var longitude = "${marketDetail.data!.location}".split("-")[1];
+              return MarketSlider(
+                images: right.data.photos,
+                marketLatitude: latitude,
+                marketLongitude: longitude,
+              );
+            },
           );
         },
         errorBuilder: (context, data) {
-          return CarouselImageSlider(images: []);
+          return MarketSlider(images: []);
         },
       ),
     );
@@ -119,7 +134,7 @@ class MarketDetailPage extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: MarketsListItem(
         market: market,
-        repository: _customerMarketRepo,
+        repository: marketCommentsNotifier.customerMarketRepo,
         onTap: () {},
       ),
     );
@@ -132,11 +147,11 @@ class MarketDetailPage extends StatelessWidget {
     );
   }
 
-  Widget _sectionComments() {
+  Widget _sectionComments(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: CustomFutureBuilder<Either<Failure, CommentsResultModel>>(
-        future: _customerMarketRepo.marketComments(
+        future: marketCommentsNotifier.customerMarketRepo.marketComments(
           marketId: marketId,
           page: 1,
         ),
@@ -146,16 +161,18 @@ class MarketDetailPage extends StatelessWidget {
               left.message,
               style: AppTxtStyles().body..alignment.center(),
             ),
-            (right) => CommentList(
-              comments: right.data!.comments!,
-              showAllCommentOnTap: () {
-                Routes.sailor.navigate(
-                  MarketCommentsPage.route,
-                  params: {"marketId": marketId},
-                );
-              },
-              enableHeader: right.data!.comments!.isNotEmpty,
-            ),
+            (right) {
+              return CommentList(
+                comments: right.data!.comments!.sublist(0, 3),
+                enableHeader: right.data!.comments!.isNotEmpty,
+                showAllCommentOnTap: () {
+                  Routes.sailor.navigate(
+                    MarketCommentsPage.route,
+                    params: {"marketId": marketId},
+                  );
+                },
+              );
+            },
           );
         },
         errorBuilder: (context, data) {
