a2342b996bfe07b4103ca580a456471bd6a388f0
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-18 22:24:03 +0430

    create google map widget

---
+++ lib/core/widgets/map_box.dart
@@ -0,0 +1,289 @@
+import 'dart:async';
+import 'dart:convert';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../services/location.dart';
+import '../themes/app_theme.dart';
+import 'loading.dart';
+
+class MapBox extends StatefulWidget {
+  MapBox({
+    required this.height,
+    this.latitude,
+    this.longitude,
+  });
+
+  final double height;
+  final String? latitude;
+  final String? longitude;
+
+  @override
+  State<MapBox> createState() => _MapBoxState();
+}
+
+class _MapBoxState extends State<MapBox> {
+  var _controller = Completer<GoogleMapController>();
+  late GoogleMapController mapController;
+
+  late BitmapDescriptor pinLocationIcon;
+  Set<Marker> markers = {};
+
+  late LatLng pinPosition;
+  late CameraPosition initialLocation;
+  late String mapStyle;
+  bool loading = true;
+
+  void initializeState() async {
+    mapStyle = ''' [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]''';
+
+    pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 2.5),
+      'assets/destination_map_marker.png',
+    );
+
+    Position position;
+    if (widget.latitude == null) {
+      position = await LocationService.getSavedLocation();
+    } else {
+      position = Position(
+        latitude: double.parse(widget.latitude!),
+        longitude: double.parse(widget.longitude!),
+      );
+    }
+
+    pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 2.5),
+      'assets/destination_map_marker.png',
+    );
+
+    pinPosition = LatLng(
+      position.latitude,
+      position.longitude,
+    );
+
+    initialLocation = CameraPosition(
+      zoom: 16,
+      bearing: 32,
+      target: pinPosition,
+    );
+
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Parent(
+      style: ParentStyle()
+        ..background.color(Colors.white)
+        ..borderRadius(all: 8)
+        ..boxShadow(
+          color: Colors.black.withOpacity(0.2),
+          offset: Offset(0, 4.0),
+          blur: 8,
+          spread: 0,
+        ),
+      child: SizedBox(
+        height: widget.height,
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
+          ),
+          child: loading
+              ? AppLoading(color: AppTheme.customerPrimary)
+              : GoogleMap(
+                  mapType: MapType.hybrid,
+                  initialCameraPosition: initialLocation,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                    mapController = controller;
+                    mapController.setMapStyle(mapStyle);
+
+                    setState(() {
+                      markers.add(
+                        Marker(
+                          markerId: MarkerId("marketId"),
+                          position: pinPosition,
+                          icon: pinLocationIcon,
+                        ),
+                      );
+                    });
+                  },
+                ),
+        ),
+      ),
+    );
+  }
+}
