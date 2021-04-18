015d9b9672775f99ec2b12ceb14f30e4d7344d63
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-18 22:28:15 +0430

    edit market sub category

---
+++ lib/features/presentation/customer_pages/market_sub_category/market_sub_category.dart
@@ -0,0 +1,78 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:dartz/dartz.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/exceptions/failure.dart';
+import '../../../../core/page_routes/routes.dart';
+import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/themes/app_theme.dart';
+import '../../../../core/widgets/custom_future_builder.dart';
+import '../../../../core/widgets/loading.dart';
+import '../../../data/models/sub_category_result_model.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/customer_providers/category_notifier.dart';
+import '../products/produtct_page.dart';
+import 'widgets/market_sub_category_list.dart';
+
+class MarketSubCategoryPage extends StatelessWidget {
+  static const route = "customer_market_sub_category";
+
+  MarketSubCategoryPage({
+    required this.marketId,
+    required this.mainCategoryId,
+  });
+
+  final String marketId;
+  final String mainCategoryId;
+
+  @override
+  Widget build(BuildContext context) {
+    var categoryNotifier =
+        Provider.of<CategoryNotifier>(context, listen: false);
+
+    return Scaffold(
+      appBar: SimpleAppBar(context).create(
+        text: "جزئیات فروشگاه",
+        showBackBtn: true,
+        showBasketBtn: true,
+      ),
+      body: CustomFutureBuilder(
+        future: categoryNotifier.customerCategoryRepo.subCategories(
+          marketId: marketId,
+          mainCategoryId: mainCategoryId,
+        ),
+        successBuilder: (context, data) {
+          var result = data as Either<Failure, SubCategoryResultModel>;
+
+          return result.fold(
+            (left) => Txt(
+              left.message,
+              style: AppTxtStyles().body..alignment.center(),
+            ),
+            (right) => MarketSubCategoryList(
+              categoryNotifier: categoryNotifier,
+              subCategories: right.data!,
+              onItemTap: (index) {
+                Routes.sailor.navigate(
+                  ProductsPage.route,
+                  params: {
+                    "marketId": marketId,
+                    "categoryId": right.data![index].id,
+                  },
+                );
+              },
+            ),
+          );
+        },
+        errorBuilder: (context, error) {
+          return AppLoading(
+            color: AppTheme.customerPrimary,
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/features/presentation/customer_pages/market_sub_category/widgets/market_sub_category_list_item.dart
@@ -0,0 +1,95 @@
+import 'dart:convert';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:dartz/dartz.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/exceptions/failure.dart';
+import '../../../../../core/styles/txt_styles.dart';
+import '../../../../../core/widgets/custom_future_builder.dart';
+import '../../../../data/models/photo_result_model.dart';
+import '../../../../data/models/sub_category_result_model.dart';
+import '../../../providers/customer_providers/category_notifier.dart';
+
+class MarketSubCategoryListItem extends StatelessWidget {
+  MarketSubCategoryListItem({
+    required this.subCategory,
+    required this.onTap,
+    required this.categoryNotifier,
+  });
+
+  final SubCategory subCategory;
+  final void Function() onTap;
+  final CategoryNotifier categoryNotifier;
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return Parent(
+      gesture: Gestures()..onTap(onTap),
+      style: ParentStyle()
+        ..padding(horizontal: 4, vertical: 4)
+        ..background.color(Colors.white)
+        ..borderRadius(all: 12)
+        ..boxShadow(
+          color: Colors.black.withOpacity(0.1),
+          offset: Offset(0, 3.0),
+          blur: 6,
+          spread: 0,
+        )
+        ..ripple(true),
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: Parent(
+              style: ParentStyle()
+                ..width(screenSize.width)
+                ..borderRadius(all: 8)
+                ..background.image(
+                  alignment: Alignment.center,
+                  path: "assets/images/placeholder.jpg",
+                  fit: BoxFit.fill,
+                ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(8),
+                ),
+                child: _futureImgFile,
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          Txt(
+            "${subCategory.name}",
+            style: AppTxtStyles().footNote
+              ..alignment.center()
+              ..textOverflow(TextOverflow.ellipsis)
+              ..maxLines(1),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget get _futureImgFile {
+    return CustomFutureBuilder<Either<Failure, PhotoResultModel>>(
+      future: categoryNotifier.customerCategoryRepo.subCategoryPhoto(
+        id: subCategory.id,
+      ),
+      successBuilder: (context, data) {
+        return data!.fold(
+          (l) => SizedBox(),
+          (r) => Image.memory(
+            base64Decode(r.data!.photo),
+            fit: BoxFit.fill,
+          ),
+        );
+      },
+      errorBuilder: (context, data) => SizedBox(),
+    );
+  }
+}
