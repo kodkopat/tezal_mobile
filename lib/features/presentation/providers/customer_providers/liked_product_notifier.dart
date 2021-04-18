575f85f3f869a995e9db02ae341f1479fd5fae36
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-18 22:26:40 +0430

    Create liked_product_notifier.dart

---
+++ lib/features/presentation/providers/customer_providers/liked_product_notifier.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/liked_products_result_model.dart';
+import '../../../data/repositories/customer_product_repository.dart';
+
+class LikedProductNotifier extends ChangeNotifier {
+  static LikedProductNotifier? _instance;
+
+  factory LikedProductNotifier(
+    CustomerProductRepository customerProductRepo,
+  ) {
+    if (_instance == null) {
+      _instance = LikedProductNotifier._privateConstructor(
+        customerProductRepo: customerProductRepo,
+      );
+    }
+
+    return _instance!;
+  }
+
+  LikedProductNotifier._privateConstructor({
+    required this.customerProductRepo,
+  });
+
+  final CustomerProductRepository customerProductRepo;
+
+  bool likedProductsLoading = true;
+  String? likedProductsErrorMsg;
+  LikedProductsResultModel? likedProductsResultModel;
+
+  Future<void> fetchLikedProducts() async {
+    var result = await customerProductRepo.likedProdutcs();
+
+    result.fold(
+      (left) => likedProductsErrorMsg = left.message,
+      (right) {
+        print("likedProducts: ${right.toJson()}\n");
+        likedProductsResultModel = right;
+      },
+    );
+
+    likedProductsLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> likeProduct({required String productId}) async {
+    var result = await customerProductRepo.likeProduct(
+      id: productId,
+    );
+
+    result.fold(
+      (left) => null,
+      (right) => refresh(),
+    );
+  }
+
+  Future<void> unlikeProduct({required String productId}) async {
+    var result = await customerProductRepo.unlikeProduct(
+      id: productId,
+    );
+
+    result.fold(
+      (left) => null,
+      (right) => refresh(),
+    );
+  }
+
+  void refresh() async {
+    await fetchLikedProducts();
+  }
+}
