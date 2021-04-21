cfeab00c3972f3fa9e66c2b5a6ba679bd6ab4080
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-21 18:41:10 +0430

    create base providers

---
+++ lib/features/base_providers.dart
@@ -0,0 +1,14 @@
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import 'presentation/providers/base_providers/direction_notifier.dart';
+import 'presentation/providers/base_providers/location_notifier.dart';
+
+List<SingleChildWidget> baseProviders = [
+  ChangeNotifierProvider(
+    create: (ctx) => LocationNotifier(),
+  ),
+  ChangeNotifierProvider(
+    create: (ctx) => DirectionNotifier(),
+  ),
+];
