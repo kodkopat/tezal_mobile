94318cc978681b9172f142f2c0042003b4f20b99
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-26 16:17:22 +0430

    add marketBaseApiUrl in consts file

--- lib/core/consts/consts.dart
+++ lib/core/consts/consts.dart
@@ -1,5 +1,6 @@
 const String sharedBaseApiUrl = "http://185.116.162.192/shared/";
 const String customerBaseApiUrl = "http://185.116.162.192/customer/";
+const String marketBaseApiUrl = "http://185.116.162.192/market/";
 
 const String storageKeyUserId = "USER_ID_STORAGE_KEY";
 const String storageKeyUserToken = "USER_TOKEN_STORAGE_KEY";
