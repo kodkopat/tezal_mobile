0296021e63ca40fea23efc95c8988dab3b91372f
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-28 19:58:42 +0430

    add Market Order Details Page route

--- lib/core/page_routes/market_routes.dart
+++ lib/core/page_routes/market_routes.dart
@@ -1,7 +1,9 @@
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:sailor/sailor.dart';
 
+import '../../features/data/models/market/market_orders_result_model.dart';
 import '../../features/presentation/market_pages/dashboard/dashboard_page.dart';
+import '../../features/presentation/market_pages/order_detail/order_detail_page.dart';
 
 void createMarketRoutes(Sailor sailor) {
   sailor.addRoutes(
@@ -9,6 +11,21 @@ void createMarketRoutes(Sailor sailor) {
       SailorRoute(
         name: DashBoardPage.route,
         builder: (ctx, args, map) => DashBoardPage(),
+      ),
+      SailorRoute(
+        name: OrderDetailPage.route,
+        builder: (ctx, args, map) {
+          final marketOrder = map.param<MarketOrder>("marketOrder");
+
+          return OrderDetailPage(marketOrder: marketOrder);
+        },
+        params: [
+          SailorParam<MarketOrder>(
+            name: "marketOrder",
+            isRequired: true,
+            defaultValue: null,
+          ),
+        ],
       ),
     ],
   );
