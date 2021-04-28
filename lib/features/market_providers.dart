b5e881a4afa49e00a809496ed58168f65255058a
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-28 19:59:40 +0430

    Update market_providers.dart

--- lib/features/market_providers.dart
+++ lib/features/market_providers.dart
@@ -1,3 +1,13 @@
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
-List<SingleChildWidget> marketProviders = [];
+import 'data/repositories/market_order_repository.dart';
+import 'presentation/providers/market_providers/order_notifier.dart';
+
+List<SingleChildWidget> marketProviders = [
+  ChangeNotifierProvider(
+    create: (ctx) => OrderNotifier(
+      MarketOrderRepository(),
+    ),
+  ),
+];
