ff37e4be484932aaa0b298dcc4e8c4da104bc4d6
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-29 19:01:30 +0430

    Create update_detail_page.dart

---
+++ lib/features/presentation/base_pages/update_detail/update_detail_page.dart
@@ -0,0 +1,41 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/widgets/loading.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/base_providers/update_notifier.dart';
+
+class UpdateDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var consumer = Consumer<UpdateNotifier>(
+      builder: (context, provider, child) {
+        if (provider.hasNewVersionResult == null) {
+          provider.fetchUpdateInfo();
+        }
+
+        return provider.hasNewVersionLoading
+            ? Center(child: AppLoading())
+            : provider.hasNewVersionResult == null
+                ? provider.hasNewVersionErrorMsg == null
+                    ? Txt("خطای بارگذاری اطلاعات",
+                        style: AppTxtStyles().body..alignment.center())
+                    : Txt(provider.hasNewVersionErrorMsg,
+                        style: AppTxtStyles().body..alignment.center())
+                : Txt(provider.hasNewVersionResult!.message,
+                    style: AppTxtStyles().body..alignment.center());
+      },
+    );
+
+    return Scaffold(
+      appBar: SimpleAppBar(context).create(
+        text: "به‌روز‌رسانی اپلیکیشن",
+        showBackBtn: true,
+      ),
+      body: consumer,
+    );
+  }
+}
