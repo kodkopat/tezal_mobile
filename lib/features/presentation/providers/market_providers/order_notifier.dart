f575049d8a4afff2cded2c396d76c5bee811763f
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-29 19:01:26 +0430

    Update order_notifier.dart

--- lib/features/presentation/providers/market_providers/order_notifier.dart
+++ lib/features/presentation/providers/market_providers/order_notifier.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/widgets/progress_dialog.dart';
+import '../../../data/models/customer/base_api_result_model.dart';
 import '../../../data/models/market/market_orders_result_model.dart';
 import '../../../data/repositories/market_order_repository.dart';
 
@@ -39,6 +41,46 @@ class OrderNotifier extends ChangeNotifier {
     );
 
     ordersLoading = false;
+    notifyListeners();
+  }
+
+  String? approveOrderErrorMsg;
+  BaseApiResultModel? approveOrderResult;
+  Future<void> approveOrder({
+    required BuildContext context,
+    required String orderId,
+  }) async {
+    var prgDialog = AppProgressDialog(context).instance;
+    prgDialog.show();
+
+    var result = await marketOrderRepo.approveOrder(id: orderId);
+
+    result.fold(
+      (left) => approveOrderErrorMsg = left.message,
+      (right) => approveOrderResult = right,
+    );
+
+    prgDialog.hide();
+    notifyListeners();
+  }
+
+  String? rejectOrderErrorMsg;
+  BaseApiResultModel? rejectOrderResult;
+  Future<void> rejectOrder({
+    required BuildContext context,
+    required String orderId,
+  }) async {
+    var prgDialog = AppProgressDialog(context).instance;
+    prgDialog.show();
+
+    var result = await marketOrderRepo.rejectOrder(id: orderId);
+
+    result.fold(
+      (left) => rejectOrderErrorMsg = left.message,
+      (right) => rejectOrderResult = right,
+    );
+
+    prgDialog.hide();
     notifyListeners();
   }
 
