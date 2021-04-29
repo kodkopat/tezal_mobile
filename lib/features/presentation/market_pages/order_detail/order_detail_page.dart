b261462903873bab222fc31cc90569b5fd16804f
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-29 19:00:07 +0430

    edit market orders

--- lib/features/presentation/market_pages/order_detail/order_detail_page.dart
+++ lib/features/presentation/market_pages/order_detail/order_detail_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../../core/page_routes/base_routes.dart';
 import '../../../data/models/market/market_orders_result_model.dart';
 import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/market_providers/order_notifier.dart';
 import '../orders/widgets/order_list_item.dart';
+import 'widgets/order_detail_action_box.dart';
 import 'widgets/order_detail_list.dart';
 
 class OrderDetailPage extends StatelessWidget {
@@ -14,6 +18,8 @@ class OrderDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var orderNotifier = Provider.of<OrderNotifier>(context, listen: false);
+
     return Scaffold(
       appBar: SimpleAppBar(context).create(
         text: "جزئیات سفارش",
@@ -31,6 +37,22 @@ class OrderDetailPage extends StatelessWidget {
             OrderDetailList(
               marketOrderItems: marketOrder.items!,
               onItemTap: (index) {},
+            ),
+            OrderDetailActionBox(
+              onApproveOrder: () async {
+                await orderNotifier.approveOrder(
+                  context: context,
+                  orderId: "marketOrder.id",
+                );
+                Routes.sailor.pop();
+              },
+              onRejectOrder: () async {
+                await orderNotifier.rejectOrder(
+                  context: context,
+                  orderId: "marketOrder.id",
+                );
+                Routes.sailor.pop();
+              },
             ),
           ],
         ),
---
+++ lib/features/presentation/market_pages/order_detail/widgets/order_detail_action_box.dart
@@ -0,0 +1,61 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/styles/txt_styles.dart';
+
+class OrderDetailActionBox extends StatelessWidget {
+  OrderDetailActionBox({
+    required this.onApproveOrder,
+    required this.onRejectOrder,
+  });
+
+  final void Function() onApproveOrder;
+  final void Function() onRejectOrder;
+
+  final _approveTxtStyle = AppTxtStyles().body
+    ..height(48)
+    ..borderRadius(all: 8)
+    ..textColor(Colors.white)
+    ..background.color(Colors.green)
+    ..textAlign.center()
+    ..alignmentContent.center()
+    ..ripple(true);
+
+  final _rejectTxtStyle = AppTxtStyles().body
+    ..height(48)
+    ..borderRadius(all: 8)
+    ..textColor(Colors.white)
+    ..background.color(Colors.red)
+    ..textAlign.center()
+    ..alignmentContent.center()
+    ..ripple(true);
+
+  @override
+  Widget build(BuildContext context) {
+    return Parent(
+      style: ParentStyle()..margin(horizontal: 16, vertical: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Txt(
+              "تایید سفارش",
+              gesture: Gestures()..onTap(onApproveOrder),
+              style: _approveTxtStyle,
+            ),
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            flex: 1,
+            child: Txt(
+              "لغو سفارش",
+              gesture: Gestures()..onTap(onRejectOrder),
+              style: _rejectTxtStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
