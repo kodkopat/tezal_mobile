27b49709d7d98f0b1feab069b31e3a7acf0f9bfa
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-04-29 19:01:03 +0430

    Update base_providers.dart

--- lib/features/base_providers.dart
+++ lib/features/base_providers.dart
@@ -1,11 +1,18 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
+import 'data/repositories/shared_application_repository.dart';
 import 'presentation/providers/base_providers/contacts_notifier.dart';
 import 'presentation/providers/base_providers/direction_notifier.dart';
 import 'presentation/providers/base_providers/location_notifier.dart';
+import 'presentation/providers/base_providers/update_notifier.dart';
 
 List<SingleChildWidget> baseProviders = [
+  ChangeNotifierProvider(
+    create: (ctx) => UpdateNotifier(
+      SharedApplicationRepository(),
+    ),
+  ),
   ChangeNotifierProvider(
     create: (ctx) => ContactsNotifier(),
   ),
