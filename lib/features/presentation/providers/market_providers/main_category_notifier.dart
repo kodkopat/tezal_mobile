c8c5e236393126d38cc6da04818dc61d704011df
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-01 20:12:27 +0430

    Create main_category_notifier.dart

---
+++ lib/features/presentation/providers/market_providers/main_category_notifier.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/market/main_categories_result_model.dart';
+import '../../../data/repositories/market_product_repository.dart';
+
+class MainCategoryNotifier extends ChangeNotifier {
+  static MainCategoryNotifier? _instance;
+
+  factory MainCategoryNotifier(
+    MarketProductRepository marketProductRepo,
+  ) {
+    if (_instance == null) {
+      _instance = MainCategoryNotifier._privateConstructor(
+        marketProductRepo: marketProductRepo,
+      );
+    }
+
+    return _instance!;
+  }
+
+  MainCategoryNotifier._privateConstructor({
+    required this.marketProductRepo,
+  });
+
+  final MarketProductRepository marketProductRepo;
+
+  bool mainCategoriesLoading = true;
+  String? mainCategoriesErrorMsg;
+  List<MainCategory>? mainCategories;
+
+  Future<void> fetchMainCategories() async {
+    var result = await marketProductRepo.getMainCategories();
+
+    result.fold(
+      (left) => mainCategoriesErrorMsg = left.message,
+      (right) => mainCategories = right.data,
+    );
+
+    mainCategoriesLoading = false;
+    notifyListeners();
+  }
+}
