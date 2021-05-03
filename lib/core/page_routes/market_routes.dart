4d6a0057fe36856cedf6bf6d20f2af5e0c5f0a5e
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-03 19:27:35 +0430

    Update market_routes.dart

--- lib/core/page_routes/market_routes.dart
+++ lib/core/page_routes/market_routes.dart
@@ -4,6 +4,12 @@ import 'package:sailor/sailor.dart';
 import '../../features/data/models/market/orders_result_model.dart';
 import '../../features/presentation/market_pages/dashboard/dashboard_page.dart';
 import '../../features/presentation/market_pages/order_detail/order_detail_page.dart';
+import '../../features/presentation/market_pages/orders/orders_page.dart';
+import '../../features/presentation/market_pages/products/products_page.dart';
+import '../../features/presentation/market_pages/profile/profile_page.dart';
+import '../../features/presentation/market_pages/wallet/wallet_page.dart';
+import '../../features/presentation/market_pages/wallet_withdrawal/wallet_withdrawal_page.dart';
+import '../../features/presentation/market_pages/wallet_withdrawal_requests/wallet_withdrawal_requests_page.dart';
 
 void createMarketRoutes(Sailor sailor) {
   sailor.addRoutes(
@@ -11,6 +17,22 @@ void createMarketRoutes(Sailor sailor) {
       SailorRoute(
         name: DashBoardPage.route,
         builder: (ctx, args, map) => DashBoardPage(),
+      ),
+      SailorRoute(
+        name: OrdersPage.route,
+        builder: (ctx, args, map) => OrdersPage(),
+      ),
+      SailorRoute(
+        name: ProductsPage.route,
+        builder: (ctx, args, map) => ProductsPage(),
+      ),
+      SailorRoute(
+        name: WalletPage.route,
+        builder: (ctx, args, map) => WalletPage(),
+      ),
+      SailorRoute(
+        name: ProfilePage.route,
+        builder: (ctx, args, map) => ProfilePage(),
       ),
       SailorRoute(
         name: OrderDetailPage.route,
@@ -26,6 +48,14 @@ void createMarketRoutes(Sailor sailor) {
             defaultValue: null,
           ),
         ],
+      ),
+      SailorRoute(
+        name: WithdrawalWalletPage.route,
+        builder: (ctx, args, map) => WithdrawalWalletPage(),
+      ),
+      SailorRoute(
+        name: WalletWithdrawalRequestsPage.route,
+        builder: (ctx, args, map) => WalletWithdrawalRequestsPage(),
       ),
     ],
   );
