95f5a754da5527e32212ef6983984fea0f3b36d2
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-08 20:23:43 +0430

    create edit profile menu page

---
+++ lib/features/presentation/market_pages/profile_info/profile_info_page.dart
@@ -0,0 +1,108 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/page_routes/base_routes.dart';
+import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/widgets/action_btn.dart';
+import '../../../../core/widgets/loading.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/market_providers/profile_notifier.dart';
+import '../profile_edit/edit_profile_page.dart';
+
+class ProfileInfoPage extends StatefulWidget {
+  static const route = "/market_info_profile";
+
+  @override
+  _ProfileInfoPageState createState() => _ProfileInfoPageState();
+}
+
+class _ProfileInfoPageState extends State<ProfileInfoPage> {
+  String errorTxt = "";
+  bool errorVisibility = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var consumer = Consumer<ProfileNotifier>(
+      builder: (context, provider, child) {
+        if (!provider.wasFetchInfoCalled) {
+          provider.fetchInfo();
+        }
+
+        return provider.infoLoading
+            ? Center(child: AppLoading())
+            : provider.infoResult == null
+                ? provider.infoErrorMsg == null
+                    ? Txt("خطای بارگذاری اطلاعات",
+                        style: AppTxtStyles().body..alignment.center())
+                    : Txt(provider.infoErrorMsg,
+                        style: AppTxtStyles().body..alignment.center())
+                : SingleChildScrollView(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 16),
+                        Txt(
+                            "نام فروشگاه: " +
+                                "${provider.infoResult!.data!.name}",
+                            style: AppTxtStyles().body),
+                        const SizedBox(height: 16),
+                        Txt(
+                            "شماره موبایل: " +
+                                "${provider.infoResult!.data!.phone}",
+                            style: AppTxtStyles().body),
+                        const SizedBox(height: 16),
+                        Txt(
+                            "شماره تلفن: " +
+                                "${provider.infoResult!.data!.telephone}",
+                            style: AppTxtStyles().body),
+                        const SizedBox(height: 16),
+                        Txt(
+                            "ایمیل آدرس: " +
+                                "${provider.infoResult!.data!.email}",
+                            style: AppTxtStyles().body),
+                        const SizedBox(height: 16),
+                        Txt("نشانی: " + "${provider.infoResult!.data!.address}",
+                            style: AppTxtStyles().body),
+                        const SizedBox(height: 16),
+                        ActionBtn(
+                          text: "ویرایش",
+                          onTap: () {
+                            Routes.sailor.navigate(
+                              EditProfilePage.route,
+                              params: {"marketProfile": provider.infoResult!},
+                            );
+                          },
+                        ),
+                        const SizedBox(height: 16),
+                        Visibility(
+                          visible: errorVisibility,
+                          child: Txt(
+                            errorTxt,
+                            style: AppTxtStyles().footNote.clone()
+                              ..textColor(Theme.of(context).errorColor)
+                              ..alignmentContent.center()
+                              ..height(24)
+                              ..borderRadius(all: 2)
+                              ..padding(horizontal: 4)
+                              ..ripple(true),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
+                  );
+      },
+    );
+
+    return Scaffold(
+      appBar: SimpleAppBar(context).create(
+        text: "اطلاعات فروشگاه",
+        showBackBtn: true,
+      ),
+      body: consumer,
+    );
+  }
+}
