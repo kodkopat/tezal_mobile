b40a56342f242c77139ff254f71fc2056bf13505
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-08 20:21:40 +0430

    Update market_providers.dart

--- lib/features/market_providers.dart
+++ lib/features/market_providers.dart
@@ -3,10 +3,12 @@ import 'package:provider/single_child_widget.dart';
 
 import 'data/repositories/market_order_repository.dart';
 import 'data/repositories/market_product_repository.dart';
+import 'data/repositories/market_repository.dart';
 import 'data/repositories/market_wallet_repository.dart';
 import 'presentation/providers/market_providers/main_category_notifier.dart';
 import 'presentation/providers/market_providers/order_notifier.dart';
 import 'presentation/providers/market_providers/products_notifier.dart';
+import 'presentation/providers/market_providers/profile_notifier.dart';
 import 'presentation/providers/market_providers/wallet_notifier.dart';
 
 List<SingleChildWidget> marketProviders = [
@@ -28,6 +30,11 @@ List<SingleChildWidget> marketProviders = [
   ChangeNotifierProvider(
     create: (ctx) => WalletNotifier(
       MarketWalletRepository(),
+    ),
+  ),
+  ChangeNotifierProvider(
+    create: (ctx) => ProfileNotifier(
+      MarketRepository(),
     ),
   ),
 ];
