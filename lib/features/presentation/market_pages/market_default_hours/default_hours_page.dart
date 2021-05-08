5c3f2bc7eef143b0c98201853c8e5a504a530d4b
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-08 20:22:53 +0430

    create market default hours page & it's widgets

---
+++ lib/features/presentation/market_pages/market_default_hours/default_hours_page.dart
@@ -0,0 +1,70 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/page_routes/base_routes.dart';
+import '../../../../core/styles/txt_styles.dart';
+import '../../../../core/widgets/action_btn.dart';
+import '../../../../core/widgets/loading.dart';
+import '../../customer_widgets/simple_app_bar.dart';
+import '../../providers/market_providers/profile_notifier.dart';
+import '../market_default_hours_edit/edit_default_hours_page.dart';
+import 'widgets/default_hours_list.dart';
+
+class DefaultHoursPage extends StatelessWidget {
+  static const route = "/market_default_hours";
+
+  @override
+  Widget build(BuildContext context) {
+    var consumer = Consumer<ProfileNotifier>(
+      builder: (context, provider, child) {
+        if (!provider.wasFetchDefaultHoursCalled) {
+          provider.fetchDefaultHours();
+        }
+
+        return provider.defaultHoursLoading
+            ? Center(child: AppLoading())
+            : provider.defaultHoursResult == null
+                ? provider.defaultHoursErrorMsg == null
+                    ? Txt("خطای بارگذاری اطلاعات",
+                        style: AppTxtStyles().body..alignment.center())
+                    : Txt(provider.defaultHoursErrorMsg,
+                        style: AppTxtStyles().body..alignment.center())
+                : SingleChildScrollView(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MarketDefaultHoursList(
+                          marketDefaultHours:
+                              provider.defaultHoursResult!.data!,
+                        ),
+                        const SizedBox(height: 16),
+                        ActionBtn(
+                          text: "ویرایش",
+                          onTap: () {
+                            Routes.sailor.navigate(
+                              EditDefaultHoursPage.route,
+                              params: {
+                                "marketDefaultHours":
+                                    provider.defaultHoursResult
+                              },
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+      },
+    );
+
+    return Scaffold(
+      appBar: SimpleAppBar(context).create(
+        text: "ساعات کاری فروشگاه",
+        showBackBtn: true,
+      ),
+      body: consumer,
+    );
+  }
+}
