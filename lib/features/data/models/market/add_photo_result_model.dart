830d98968b1e89d126cda9aeed209a6efb086d18
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-10 23:37:33 +0430

    Create add_photo_result_model.dart

---
+++ lib/features/data/models/market/add_photo_result_model.dart
@@ -0,0 +1,40 @@
+class AddPhotoResultModel {
+  AddPhotoResultModel({
+    required this.success,
+    required this.message,
+    required this.data,
+  });
+
+  final success;
+  final message;
+  final Data? data;
+
+  factory AddPhotoResultModel.fromJson(Map<String, dynamic> json) =>
+      AddPhotoResultModel(
+        success: json["success"],
+        message: json["message"],
+        data: json["data"] == null ? null : Data.fromJson(json["data"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
+        "data": data == null ? null : data!.toJson(),
+      };
+}
+
+class Data {
+  Data({
+    required this.photoId,
+  });
+
+  final photoId;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        photoId: json["photoId"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "photoId": photoId,
+      };
+}
