122b1e110a636ce6cad1412a336018c394ee9aec
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-12 20:39:53 +0430

    create 2 new data model

---
+++ lib/features/data/models/market/order_comments_result_model.dart
@@ -0,0 +1,85 @@
+class OrderCommentsResultModel {
+  OrderCommentsResultModel({
+    required this.success,
+    required this.message,
+    required this.data,
+  });
+
+  final success;
+  final message;
+  final Data? data;
+
+  factory OrderCommentsResultModel.fromJson(Map<String, dynamic> json) =>
+      OrderCommentsResultModel(
+        success: json["success"],
+        message: json["message"],
+        data: json["data"] == null ? null : Data.fromJson(json["data"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
+        "data": data == null ? null : data!.toJson(),
+      };
+}
+
+class Data {
+  Data({
+    required this.totalCount,
+    required this.result,
+  });
+
+  final totalCount;
+  final List<OrderComment>? result;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        totalCount: json["totalCount"],
+        result: json["result"] == null
+            ? null
+            : List<OrderComment>.from(
+                json["result"].map((x) => OrderComment.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "totalCount": totalCount,
+        "result": result == null
+            ? null
+            : List<dynamic>.from(result!.map((x) => x.toJson())),
+      };
+}
+
+class OrderComment {
+  OrderComment({
+    required this.id,
+    required this.comment,
+    required this.createDate,
+    required this.rate,
+    required this.orderId,
+    required this.reply,
+  });
+
+  final id;
+  final comment;
+  final createDate;
+  final rate;
+  final orderId;
+  final reply;
+
+  factory OrderComment.fromJson(Map<String, dynamic> json) => OrderComment(
+        id: json["id"],
+        comment: json["comment"],
+        createDate: json["createDate"],
+        rate: json["rate"],
+        orderId: json["orderId"],
+        reply: json["reply"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "comment": comment,
+        "createDate": createDate,
+        "rate": rate,
+        "orderId": orderId,
+        "reply": reply,
+      };
+}
