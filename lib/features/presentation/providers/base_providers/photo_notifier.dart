06f22253a4bf4780d65b9800d0dc10ef89a59911
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-15 17:29:36 +0430

    Create photo_notifier.dart

---
+++ lib/features/presentation/providers/base_providers/photo_notifier.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:tezal/features/data/models/photo_model.dart';
+
+import '../../../data/repositories/shared_application_repository.dart';
+
+class PhotoNotifier extends ChangeNotifier {
+  static PhotoNotifier? _instance;
+
+  factory PhotoNotifier(
+    SharedApplicationRepository sharedApplicationRepo,
+  ) {
+    if (_instance == null) {
+      _instance = PhotoNotifier._privateConstructor(sharedApplicationRepo);
+    }
+
+    return _instance!;
+  }
+
+  PhotoNotifier._privateConstructor(this.sharedApplicationRepo);
+
+  final SharedApplicationRepository sharedApplicationRepo;
+
+  bool loading = true;
+  String? errorMsg;
+  PhotoModel? result;
+
+  Future<void> fetchPhoto({
+    required String productId,
+  }) async {
+    var result = await sharedApplicationRepo.getProductPhoto(
+      productId: productId,
+    );
+
+    result.fold(
+      (left) => errorMsg = left.message,
+      (right) => result = right.data!.photo,
+    );
+
+    loading = false;
+    notifyListeners();
+  }
+}
