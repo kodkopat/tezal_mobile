4bc83182c49921e922aa8df39262f4bb6cf0f18c
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-24 22:47:27 +0430

    Update withdrawal_request_list_item.dart

--- lib/features/presentation/market_pages/wallet_withdrawal_requests/widgets/withdrawal_request_list_item.dart
+++ lib/features/presentation/market_pages/wallet_withdrawal_requests/widgets/withdrawal_request_list_item.dart
@@ -1,9 +1,11 @@
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
 
 import '../../../../../core/styles/txt_styles.dart';
 import '../../../../data/models/market/withdrawal_requests_result_model.dart';
+import '../../../customer_widgets/custom_rich_text.dart';
 
 class WithdrawalRequestListItem extends StatelessWidget {
   WithdrawalRequestListItem({required this.withdrawalRequest});
@@ -16,76 +18,66 @@ class WithdrawalRequestListItem extends StatelessWidget {
       style: ParentStyle()
         ..margin(vertical: 8)
         ..padding(horizontal: 8, vertical: 8),
-      child: Txt(
-        "${withdrawalRequest.description}",
-        style: AppTxtStyles().body..textAlign.start(),
-      )
-      /* Row(
-        textDirection: TextDirection.rtl,
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
-            textDirection: TextDirection.rtl,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _fieldTransactionType,
-              _fieldTransactionDate,
+              _fieldDate(context),
+              _fieldDescription(context),
             ],
           ),
-          _fieldTransactionPriceText,
+          _fieldAmount,
         ],
-      ) */
-      ,
+      ),
     );
   }
 
-  // Widget get _fieldTransactionType {
-  //   return CustomRichText(
-  //     title: "نوع تراکنش: ",
-  //     text: "${withdrawalRequest.walletActionType}",
-  //   );
-  // }
+  Widget _fieldDescription(BuildContext context) {
+    return CustomRichText(
+      title: "توضیحات" + ": ",
+      text: "${withdrawalRequest.description}",
+    );
+  }
 
-  // Widget get _fieldTransactionDate {
-  //   return CustomRichText(
-  //     title: "تاریخ تراکنش: ",
-  //     text: _generateTransactionDateText(),
-  //   );
-  // }
+  Widget _fieldDate(BuildContext context) {
+    return CustomRichText(
+      title: "تاریخ" + ": ",
+      text: _generateDateText(),
+    );
+  }
 
-  // String _generateTransactionDateText() {
-  //   return DateTime.parse(withdrawalRequest.createDate)
-  //       .toIso8601String()
-  //       .split("T")[0]
-  //       .replaceAll("-", "/");
-  // }
+  String _generateDateText() {
+    return "${withdrawalRequest.createDate}".split(" ")[0];
+  }
 
-  // Widget get _fieldTransactionPriceText {
-  //   return Txt(
-  //     _generateTransactionPriceText(),
-  //     style: AppTxtStyles().subHeading
-  //       ..textColor("${withdrawalRequest.amount}".contains("-")
-  //           ? Colors.red
-  //           : Colors.green)
-  //       ..bold(),
-  //   );
-  // }
+  Widget get _fieldAmount {
+    return Txt(
+      _generateAmount(),
+      style: AppTxtStyles().subHeading
+        ..textColor("${withdrawalRequest.amount}".contains("-")
+            ? Colors.red
+            : Colors.green)
+        ..bold(),
+    );
+  }
 
-  // String _generateTransactionPriceText() {
-  //   String priceTxt;
-  //   if (withdrawalRequest.amount == null) {
-  //     priceTxt = " نامشخص ";
-  //   } else {
-  //     var temp;
-  //     if ("${withdrawalRequest.amount}".length >= 3) {
-  //       temp = intl.NumberFormat("#,000").format(withdrawalRequest.amount);
-  //     } else {
-  //       temp = "${withdrawalRequest.amount}";
-  //     }
+  String _generateAmount() {
+    String priceTxt;
+    if (withdrawalRequest.amount == null) {
+      priceTxt = " نامشخص ";
+    } else {
+      var temp;
+      if ("${withdrawalRequest.amount}".length >= 3) {
+        temp = intl.NumberFormat("#,000").format(withdrawalRequest.amount);
+      } else {
+        temp = "${withdrawalRequest.amount}";
+      }
 
-  //     priceTxt = " $temp " + "تومان";
-  //   }
+      priceTxt = " $temp " + "تومان";
+    }
 
-  //   return priceTxt.replaceAll("-", "");
-  // }
+    return priceTxt.replaceAll("-", "");
+  }
 }
