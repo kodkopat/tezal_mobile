29653d8894f0fa9ea1762760878d88a48bbf1354
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-26 20:05:00 +0430

    Update base_providers.dart

--- lib/features/presentation/base_providers.dart
+++ lib/features/presentation/base_providers.dart
@@ -2,9 +2,7 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import '../data/repositories/shared_application_repository.dart';
-import 'base_providers/direction_notifier.dart';
 import 'base_providers/location_notifier.dart';
-import 'base_providers/photo_notifier.dart';
 import 'base_providers/update_notifier.dart';
 
 List<SingleChildWidget> baseProviders = [
@@ -14,14 +12,6 @@ List<SingleChildWidget> baseProviders = [
     ),
   ),
   ChangeNotifierProvider(
-    create: (ctx) => PhotoNotifier(
-      SharedApplicationRepository(),
-    ),
-  ),
-  ChangeNotifierProvider(
     create: (ctx) => LocationNotifier(),
-  ),
-  ChangeNotifierProvider(
-    create: (ctx) => DirectionNotifier(),
   ),
 ];
