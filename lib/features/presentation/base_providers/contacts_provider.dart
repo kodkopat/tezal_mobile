b4b4f1df25f005afac51dda97eff8edad84f393d
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-26 20:05:52 +0430

    Update contacts_provider.dart

--- lib/features/presentation/base_providers/contacts_provider.dart
+++ lib/features/presentation/base_providers/contacts_provider.dart
@@ -6,41 +6,89 @@ import '../../../core/widgets/progress_dialog.dart';
 import '../../data/models/base_api_result_model.dart';
 import '../../data/repositories/shared_application_repository.dart';
 
-class ContactsStateNotifier extends StateNotifier {
-  ContactsStateNotifier(this.sharedApplicationRepo)
-      : super(sharedApplicationRepo);
+class ContactsStateNotifier extends ChangeNotifier {
+  ContactsStateNotifier(this.sharedApplicationRepo);
 
   final SharedApplicationRepository sharedApplicationRepo;
 
-  Future<List<Contact>?> fetchContacts() async {
+  bool wasFetchContactsCalled = false;
+  bool contactsLoading = true;
+  String? contactsErrorMsg;
+
+  int contactsSkip = 0;
+  int contactsTake = 10;
+  int? contactsTotalCount;
+  bool? contactsEnableLoadMoreData;
+  List<Contact> allContacts = [];
+  List<Contact>? paginatedContacts;
+
+  Future<void> fetchContacts(BuildContext context) async {
     if (await FlutterContacts.requestPermission()) {
-      var contacts = await FlutterContacts.getContacts(
-        withProperties: true,
-        withPhoto: false,
-      );
+      if (!wasFetchContactsCalled) {
+        wasFetchContactsCalled = true;
+        notifyListeners();
+      }
 
-      var contactsTemp = <Contact>[];
+      if (contactsTotalCount == null) {
+        var result = await FlutterContacts.getContacts(
+          withProperties: true,
+          withPhoto: false,
+        );
 
-      contacts.forEach((element) {
-        if (element.phones.isEmpty) {
-          // remove contact that have no any phone number
-        } else if (element.phones.length > 1) {
-          // split contact that have more than one phone number
-          for (int i = 0; i < element.phones.length; i++) {
-            contactsTemp.add(
-              Contact(
-                name: element.name,
-                displayName: element.displayName,
-                phones: [element.phones[i]],
-              ),
-            );
+        result.forEach((element) {
+          if (element.phones.isEmpty) {
+            // remove contact that have no any phone number
+          } else if (element.phones.length > 1) {
+            // split contact that have more than one phone number
+            for (int i = 0; i < element.phones.length; i++) {
+              allContacts.add(
+                Contact(
+                  name: element.name,
+                  displayName: element.displayName,
+                  phones: [element.phones[i]],
+                ),
+              );
+            }
+          } else {
+            allContacts.add(element);
           }
+        });
+
+        contactsTotalCount = allContacts.length;
+        paginatedContacts =
+            allContacts.sublist(contactsSkip, contactsSkip + contactsTake);
+        contactsEnableLoadMoreData =
+            contactsTotalCount != paginatedContacts!.length;
+        contactsSkip += contactsTake;
+
+        contactsLoading = false;
+        notifyListeners();
+      } else {
+        if (contactsTotalCount == 0) return;
+
+        var prgDialog = AppProgressDialog(context).instance;
+        await prgDialog.show();
+
+        if (allContacts.length - paginatedContacts!.length >= contactsTake) {
+          paginatedContacts!.addAll(
+              allContacts.sublist(contactsSkip, contactsSkip + contactsTake));
         } else {
-          contactsTemp.add(element);
+          paginatedContacts!.addAll(allContacts.sublist(contactsSkip,
+              contactsSkip + allContacts.length - paginatedContacts!.length));
         }
-      });
-      print("contactsLength: ${contactsTemp.length}\n");
-      return contactsTemp;
+
+        contactsEnableLoadMoreData =
+            contactsTotalCount != paginatedContacts!.length;
+
+        print("contactsTotalCount: $contactsTotalCount\n");
+        print("paginatedContactsCount: ${paginatedContacts!.length}\n");
+
+        contactsSkip += contactsTake;
+
+        await prgDialog.hide();
+
+        notifyListeners();
+      }
     } else {
       return null;
     }
@@ -69,13 +117,13 @@ class ContactsStateNotifier extends StateNotifier {
   }
 }
 
-final contactsProvider = Provider(
+final contactsProvider = ChangeNotifierProvider(
   (ref) => ContactsStateNotifier(
     SharedApplicationRepository(),
   ),
 );
 
-final contactsFutureProvider = FutureProvider.autoDispose((ref) async {
-  var provider = ref.read(contactsProvider);
-  return provider.fetchContacts();
-});
+// final contactsFutureProvider = FutureProvider.autoDispose((ref) async {
+//   var provider = ref.read(contactsProvider);
+//   return provider.fetchContacts();
+// });
