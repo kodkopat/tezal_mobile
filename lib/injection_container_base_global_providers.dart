9da75e16829a734fc9115829e198fb59ac8231b1
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-05-29 17:20:09 +0430

    Create injection_container_base_global_providers.dart

---
+++ lib/injection_container_base_global_providers.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'features/data/repositories/auth_repository.dart';
+
+import 'features/data/repositories/shared_application_repository.dart';
+import 'features/presentation/app_notifier.dart';
+import 'features/presentation/base_providers/contacts_provider.dart';
+import 'features/presentation/base_providers/update_notifier.dart';
+
+Future<void> initBaseProviders() async {
+  Get.put<AppNotifier>(
+    AppNotifier(
+      Get.find<AuthRepository>(),
+    ),
+  );
+
+  Get.put<UpdateNotifier>(
+    UpdateNotifier(
+      Get.find<SharedApplicationRepository>(),
+    ),
+  );
+
+  Get.put<ContactsStateNotifier>(
+    ContactsStateNotifier(
+      Get.find<SharedApplicationRepository>(),
+    ),
+  );
+}
