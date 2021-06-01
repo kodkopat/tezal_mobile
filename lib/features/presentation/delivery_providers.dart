5e280eef3c3c6b893170e9b57411c1c1fbf7f09d
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-06-01 20:10:41 +0430

    Update delivery_providers.dart

--- lib/features/presentation/delivery_providers.dart
+++ lib/features/presentation/delivery_providers.dart
@@ -1,3 +1,17 @@
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
-List<SingleChildWidget> deliveryProviders = [];
+import '../data/repositories/delivery_wallet_repository.dart';
+import 'delivery_providers/profile_notifier.dart';
+import 'delivery_providers/wallet_notifier.dart';
+
+List<SingleChildWidget> deliveryProviders = [
+  ChangeNotifierProvider(
+    create: (ctx) => DeliveryWalletNotifier(
+      DeliveryWalletRepository(),
+    ),
+  ),
+  ChangeNotifierProvider(
+    create: (ctx) => DeliveryProfileNotifier(),
+  ),
+];
