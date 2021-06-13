95b944d92fc2127b5ca262956eb2001de7bd066d
ArminMohamadi <44408029+ArminMohamadi@users.noreply.github.com>
2021-06-13 21:37:34 +0430

    replace base API URL with raw string

--- lib/core/consts/consts.dart
+++ lib/core/consts/consts.dart
@@ -5,13 +5,13 @@ const String marketBaseApiUrl = "http://185.116.162.30/market/";
 const String deliveryBaseApiUrl = "http://185.116.162.30/delivery/";
 */
 
-const String sharedBaseApiUrl = "http://185.116.162.192/shared/";
-const String customerBaseApiUrl = "http://185.116.162.192/customer/";
-const String marketBaseApiUrl = "http://185.116.162.192/market/";
-const String deliveryBaseApiUrl = "http://185.116.162.192/delivery/";
+const String sharedBaseApiUrl = r"http://185.116.162.192/shared/";
+const String customerBaseApiUrl = r"http://185.116.162.192/customer/";
+const String marketBaseApiUrl = r"http://185.116.162.192/market/";
+const String deliveryBaseApiUrl = r"http://185.116.162.192/delivery/";
 
-const String storageKeyUserId = "USER_ID_STORAGE_KEY";
-const String storageKeyUserToken = "USER_TOKEN_STORAGE_KEY";
-const String storageKeyUserType = "USER_TYPE_STORAGE_KEY";
-const String storageKeyUserLocation = "USER_LOCATION_STORAGE_KEY";
-const String storageKeyLocalCode = "LOCAL_CODE_STORAGE_KEY";
+const String storageKeyUserId = r"USER_ID_STORAGE_KEY";
+const String storageKeyUserToken = r"USER_TOKEN_STORAGE_KEY";
+const String storageKeyUserType = r"USER_TYPE_STORAGE_KEY";
+const String storageKeyUserLocation = r"USER_LOCATION_STORAGE_KEY";
+const String storageKeyLocalCode = r"LOCAL_CODE_STORAGE_KEY";
